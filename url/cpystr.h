c30118f3439d74737a642ad7472eb8b27d00e99a
Alain Magloire <alainm@gnu.org>
1999-12-15 07:02:35 +0000

    cpystr.h rename url0.h to cpystr.h this should be move to lib, later

---
+++ url/cpystr.h
@@ -0,0 +1,30 @@
+/* GNU mailutils - a suite of utilities for electronic mail
+   Copyright (C) 1999 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Library Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#ifndef _URL0_H
+#define _URL0_H
+
+#ifndef __P
+# ifdef __STDC__
+#  define __P(x)  x
+# else
+#  define __P(x)
+# endif
+#endif
+extern int _cpystr __P ((const char *src, char *dst, unsigned int size));
+
+#endif
