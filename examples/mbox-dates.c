77037aee35b3e0cb54b60fce9c680b6fbfb4eb8c
Sam Roberts <sroberts@uniserve.com>
2001-06-15 00:06:34 +0000

    GNU indented.

--- examples/mbox-dates.c
+++ examples/mbox-dates.c
@@ -16,24 +16,29 @@
 #include <mailutils/parse822.h>
 
 
-static const char* UserAgent(header_t hdr)
+static const char *
+UserAgent (header_t hdr)
 {
   static char agent[128];
   size_t sz;
 
-  if(header_get_value(hdr, "User-Agent", agent, sizeof(agent), &sz) == 0 && sz != 0)
+  if (header_get_value (hdr, "User-Agent", agent, sizeof (agent), &sz) == 0
+      && sz != 0)
     return agent;
 
-  if(header_get_value(hdr, "X-Mailer", agent, sizeof(agent), &sz) == 0 && sz != 0)
+  if (header_get_value (hdr, "X-Mailer", agent, sizeof (agent), &sz) == 0
+      && sz != 0)
     return agent;
 
-  if(header_get_value(hdr, "Message-Id", agent, sizeof(agent), &sz) == 0 && sz != 0)
+  if (header_get_value (hdr, "Message-Id", agent, sizeof (agent), &sz) == 0
+      && sz != 0)
     return agent;
 
   return "unknown";
 }
+
 int
-main(int argc, char **argv)
+main (int argc, char **argv)
 {
   mailbox_t mbox;
   size_t i;
@@ -41,7 +46,7 @@ main(int argc, char **argv)
   char *mboxname = argv[1];
   int status;
 
-  /* Register the desire formats.  */
+  /* Register desired mailbox formats. */
   {
     list_t bookie;
     registrar_get_list (&bookie);
@@ -49,11 +54,11 @@ main(int argc, char **argv)
   }
 
   if ((status = mailbox_create_default (&mbox, mboxname)) != 0)
-  {
-    fprintf (stderr, "could not create <%s>: %s\n",
-      mboxname, strerror (status));
-    exit (1);
-  }
+    {
+      fprintf (stderr, "could not create <%s>: %s\n",
+	       mboxname, strerror (status));
+      exit (1);
+    }
 
   {
     debug_t debug;
@@ -62,56 +67,50 @@ main(int argc, char **argv)
   }
 
   if ((status = mailbox_open (mbox, MU_STREAM_READ)) != 0)
-  {
-    fprintf (stderr, "could not open mbox: %s\n", strerror (status));
-    exit (1);
-  }
+    {
+      fprintf (stderr, "could not open mbox: %s\n", strerror (status));
+      exit (1);
+    }
 
   mailbox_messages_count (mbox, &count);
 
   for (i = 1; i <= count; ++i)
-  {
-    message_t msg;
-    header_t hdr;
-    char mailer[128];
-    char date[128];
-    size_t len = 0;
+    {
+      message_t msg;
+      header_t hdr;
+      char date[128];
+      size_t len = 0;
 
-    if (
-      (status = mailbox_get_message (mbox, i, &msg)) != 0 ||
-      (status = message_get_header (msg, &hdr)) != 0
-      )
-    {
-      printf ("%s, msg %d: %s\n", mboxname, i, strerror(status));
-      continue;
-    }
-    if ((status = header_get_value (
-        hdr, MU_HEADER_DATE, date, sizeof (date), &len)) != 0)
-    {
-      printf ("%s, msg %d: NO DATE (mua? %s)\n",
-        mboxname, i, UserAgent(hdr));
-      continue;
-    }
-    else
-    {
-      const char* s = date;
-      struct tm   tm;
-      char        dir[] = "yyyy.mm.dd";
-      size_t      nparts = 0;
-      size_t      partno;
+      if ((status = mailbox_get_message (mbox, i, &msg)) != 0 ||
+	  (status = message_get_header (msg, &hdr)) != 0)
+	{
+	  printf ("%s, msg %d: %s\n", mboxname, i, strerror (status));
+	  continue;
+	}
+      if ((status =
+	   header_get_value (hdr, MU_HEADER_DATE, date, sizeof (date),
+			     &len)) != 0)
+	{
+	  printf ("%s, msg %d: NO DATE (mua? %s)\n",
+		  mboxname, i, UserAgent (hdr));
+	  continue;
+	}
+      else
+	{
+	  const char *s = date;
+	  struct tm tm;
 
-      if(parse822_date_time(&s, s + strlen(s), &tm))
-      {
-	printf ("%s, msg %d: BAD DATE <%s> (mua? %s)\n",
-	  mboxname, i, date, UserAgent(hdr));
-        continue;
-      }
+	  if (parse822_date_time (&s, s + strlen (s), &tm))
+	    {
+	      printf ("%s, msg %d: BAD DATE <%s> (mua? %s)\n",
+		      mboxname, i, date, UserAgent (hdr));
+	      continue;
+	    }
+	}
     }
-  }
 
   mailbox_close (mbox);
   mailbox_destroy (&mbox);
 
   return status;
 }
-
