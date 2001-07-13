ab85bbb623e13e2a1e530c7f389957b44df337fc
Jakob Kaivo <jkaivo@ndn.net>
2001-07-13 02:09:44 +0000

    new "catchall" header, includes all mailutils headers

---
+++ include/mailutils/mailutils.h
@@ -0,0 +1,42 @@
+/* GNU mailutils - a suite of utilities for electronic mail
+   Copyright (C) 1999, 2000, 2001 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Library Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#include <mailutils/address.h>
+#include <mailutils/attribute.h>
+#include <mailutils/auth.h>
+#include <mailutils/body.h>
+#include <mailutils/debug.h>
+#include <mailutils/envelope.h>
+#include <mailutils/error.h>
+#include <mailutils/filter.h>
+#include <mailutils/folder.h>
+#include <mailutils/header.h>
+#include <mailutils/iterator.h>
+#include <mailutils/list.h>
+#include <mailutils/locker.h>
+#include <mailutils/mailbox.h>
+#include <mailutils/mailer.h>
+#include <mailutils/message.h>
+#include <mailutils/mime.h>
+#include <mailutils/monitor.h>
+#include <mailutils/mutil.h>
+#include <mailutils/observer.h>
+#include <mailutils/parse822.h>
+#include <mailutils/property.h>
+#include <mailutils/registrar.h>
+#include <mailutils/stream.h>
+#include <mailutils/url.h>
