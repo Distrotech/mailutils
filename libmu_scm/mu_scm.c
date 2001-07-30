ff408af89c759fa85e45d6ef5ac6030c60f802e8
Sergey Poznyakoff <gray@gnu.org.ua>
2001-07-30 13:59:47 +0000

    Define MU-ATTRIBUTE- constants.

--- libmu_scm/mu_scm.c
+++ libmu_scm/mu_scm.c
@@ -37,13 +37,31 @@ scm_makenum (unsigned long val)
 SCM _mu_scm_mailer; /* STRING: Default mailer path. */
 SCM _mu_scm_debug;  /* NUM: Default debug level. */
 
+static struct
+{
+  char *name;
+  int value;
+} attr_kw[] = {
+  { "MU-ATTRIBUTE-ANSWERED",  MU_ATTRIBUTE_ANSWERED },
+  { "MU-ATTRIBUTE-FLAGGED",   MU_ATTRIBUTE_FLAGGED },
+  { "MU-ATTRIBUTE-DELETED",   MU_ATTRIBUTE_DELETED }, 
+  { "MU-ATTRIBUTE-DRAFT",     MU_ATTRIBUTE_DRAFT },   
+  { "MU-ATTRIBUTE-SEEN",      MU_ATTRIBUTE_SEEN },    
+  { "MU-ATTRIBUTE-READ",      MU_ATTRIBUTE_READ },    
+  { "MU-ATTRIBUTE-MODIFIED",  MU_ATTRIBUTE_MODIFIED },
+  { "MU-ATTRIBUTE-RECENT",    MU_ATTRIBUTE_RECENT },
+  { NULL, 0 }
+};
+  
+
 /* Initialize the library */
 void
 mu_scm_init ()
 {
   SCM *scm_loc;
   char *defmailer;
-
+  int i;
+  
   asprintf (&defmailer, "sendmail:%s", _PATH_SENDMAIL);
   _mu_scm_mailer = scm_makfrom0str (defmailer);
   scm_loc = SCM_CDRLOC (scm_sysintern ("mu-mailer", SCM_EOL));
@@ -53,6 +71,11 @@ mu_scm_init ()
   scm_loc = SCM_CDRLOC (scm_sysintern ("mu-debug", SCM_EOL));
   *scm_loc = _mu_scm_debug;
 
+  /* Create MU- attribute names */
+  for (i = 0; attr_kw[i].name; i++)
+    scm_sysintern(attr_kw[i].name, SCM_MAKINUM(attr_kw[i].value));
+  
+  
   mu_scm_mailbox_init ();
   mu_scm_message_init ();
   mu_scm_body_init ();
