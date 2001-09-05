fe6d6e0e601f513af7c4ade5f692ea0371f5e9a6
Sergey Poznyakoff <gray@gnu.org.ua>
2001-09-05 09:13:54 +0000

    New defines: MHOST,MPORT,etc.

--- MySql/MySql.h
+++ MySql/MySql.h
@@ -2,6 +2,14 @@
 
 #ifdef HAVE_MYSQL
 
+#define MHOST NULL              /* Hostname to connect to. NULL for UNIX
+				   socket connection */ 
+#define MPORT 0                 /* Port number to connect to. 0 means default
+				   MySQL port (3300) */
+#define MSOCKET NULL            /* Socket name to use. Valid only if connecting
+				   via UNIX sockets */
+#define MFLAGS 0                /* Special user flags. It is safe to leave
+				   this untouched */
 #define MUSER "accounts"	/* Username for mysql access */
 #define MPASS "yurpass"		/* Password for mysql access */
 #define MDB "accounts"		/* Database Name */
