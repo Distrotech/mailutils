edd6a02d1f36548b9c871e49f5dc79f5c82b29ee
Sergey Poznyakoff <gray@gnu.org.ua>
2001-09-18 06:42:39 +0000

    Pass '+' key when handling +folder option.

--- mh/mh_argp.c
+++ mh/mh_argp.c
@@ -51,7 +51,7 @@ parse_opt (int key, char *arg, struct argp_state *state)
     case ARGP_KEY_ARG:
       if (arg[0] == '+')
 	{
-	  data->handler ('f', arg+1, data->closure);
+	  data->handler ('+', arg+1, data->closure);
 	  break;
 	}
       return ARGP_ERR_UNKNOWN;
