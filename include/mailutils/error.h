2da193c4efd4ca24a61923a502cf365e609f4a63
Sergey Poznyakoff <gray@gnu.org.ua>
2001-11-05 13:38:50 +0000

    Protos for mu_.*_error_printer

--- include/mailutils/error.h
+++ include/mailutils/error.h
@@ -37,6 +37,9 @@ typedef int (*error_pfn_t) __P ((const char *fmt, va_list ap));
 extern int mu_error __P ((const char *fmt, ...));
 extern void mu_error_set_print __P ((error_pfn_t));
 
+int mu_default_error_printer __P ((const char *fmt, va_list ap));
+int mu_syslog_error_printer __P((const char *fmt, va_list ap));
+  
 #ifdef __cplusplus
 }
 #endif
