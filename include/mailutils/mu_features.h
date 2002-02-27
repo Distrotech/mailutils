d108ae518840af33214a4cf53da58f3c2e2f7b27
Sergey Poznyakoff <gray@gnu.org.ua>
2002-02-27 11:59:27 +0000

    Define system-dependent macros, used by all mailutils. Currently, it contains only __P() definition.

---
+++ include/mailutils/mu_features.h
@@ -0,0 +1,29 @@
+/* GNU mailutils - a suite of utilities for electronic mail
+   Copyright (C) 1999, 2000, 2001 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#ifndef _MAILUTILS_FEATURES_H
+#define _MAILUTILS_FEATURES_H
+
+#ifndef __P
+# if defined PROTOTYPES || (defined __STDC__ && __STDC__)
+#  define __P(Args) Args
+# else
+#  define __P(Args) ()
+# endif
+#endif
+
+#endif
