acc5a383d5d9dc8f94eb88dd052b9996958d17b2
Sam Roberts <sroberts@uniserve.com>
2002-04-16 02:26:22 +0000

    One way to get rid of the annoying warnings about asprintf() not being declared is to always call a mailutils wrapper function, which in turn calls the real function. That means the wrappers (and other functions in lib) will still compile with warnings, but the mailbox and utility code can compile clean.

---
+++ lib/mu_asprintf.c
@@ -0,0 +1,36 @@
+/*
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "mu_asprintf.h"
+
+int
+mu_vasprintf (char **result, const char *format, va_list * args)
+{
+  return vasprintf (result, format, args);
+}
+
+extern int
+mu_asprintf (char **result, const char *format, ...)
+{
+  va_list args;
+  int done;
+
+  va_start (args, format);
+  done = vasprintf (result, format, args);
+  va_end (args);
+
+  return done;
+}
---
+++ lib/mu_asprintf.h
@@ -0,0 +1,29 @@
+/*
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef MUASPRINTF_H
+#define MUASPRINTF_H
+
+#include <mailutils/mu_features.h>
+
+#include <stdarg.h>
+
+extern int mu_vasprintf __P ((char **result, const char *format, va_list * args));
+
+extern int mu_asprintf __P ((char **result, const char *format, ...));
+
+#endif
+
