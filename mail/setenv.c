07c4170223feb11624ae530efa04e100a0eb8e59
Sergey Poznyakoff <gray@gnu.org.ua>
2002-10-02 14:36:35 +0000

    New file. Implements `setenv' command: an extension allowing to manipulate the shell environment.

---
+++ mail/setenv.c
@@ -0,0 +1,58 @@
+/* GNU mailutils - a suite of utilities for electronic mail
+   Copyright (C) 1999, 2000, 2001, 2002 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#include "mail.h"
+
+extern char **environ;
+
+int
+mail_setenv (int argc, char **argv)
+{
+  if (argc == 1)
+    {
+      char **p;
+      
+      for (p = environ; *p; p++)
+	printf ("%s\n", *p);
+    }
+  else
+    {
+      int i;
+
+      /* Note: POSIX requires that the argument to putenv become part
+	 of the environment itself, hence the memory allocation. */
+
+      for (i = 1; i < argc;)
+	{
+	  char *p;
+	  
+	  if (i+1 < argc && argv[i+1][0] == '=')
+	    {
+	      asprintf (&p, "%s=%s", argv[i], argv[i+2]);
+	      i += 3;
+	    }
+	  else
+	    {
+	      p = argv[i];
+	      i++;
+	    }
+	  putenv (p);
+	}
+    }
+  return 0;
+}
+
