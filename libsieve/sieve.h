792eb4400bc6ec3508c79d87ab0caae7ccb582d1
Sergey Poznyakoff <gray@gnu.org.ua>
2002-11-08 15:53:09 +0000

    Moved publicly available declarations to <mailutils/libsieve.h>

--- libsieve/sieve.h
+++ libsieve/sieve.h
@@ -2,29 +2,34 @@
    Copyright (C) 1999, 2000, 2001, 2002 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
+   it under the terms of the GNU Lesser General Public License as published by
    the Free Software Foundation; either version 2, or (at your option)
    any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+   GNU Lesser General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
+   You should have received a copy of the GNU Lesser General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
-#include <sys/types.h>
+#include <mailutils/libsieve.h>
 
-#define sieve_error mu_error
+typedef union {
+  sieve_instr_t instr;
+  sieve_value_t val;
+} sieve_op_t;
+
+struct sieve_machine {
+  size_t progsize;
+  sieve_op_t *prog;
+};
 
 extern char *sieve_filename;
 extern int sieve_line_num;
 extern int sieve_yydebug;
 
-void *sieve_alloc (size_t size);
+#define sieve_error mu_error
 
-int sieve_open_source (const char *name);
-
-int sieve_parse (const char *name);
