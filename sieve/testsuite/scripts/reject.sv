021d56e4fae129515306bd977afaccc519b260c2
Sergey Poznyakoff <gray@gnu.org.ua>
2002-11-18 17:11:57 +0000

    Added redirect.sv and reject.sv

---
+++ sieve/testsuite/scripts/reject.sv
@@ -0,0 +1,13 @@
+# -*- sieve -*-
+# This file is part of Mailutils testsuite.
+# Copyright (C) 2002, Free Software Foundation.
+# See file COPYING for distribution conditions.
+
+require "reject";
+
+reject text:
+I don't want to read these messages.
+
+Regards.
+.
+;
