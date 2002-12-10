6d3a10d65c831336dffc2a559130b8af1b794088
Sergey Poznyakoff <gray@gnu.org.ua>
2002-12-10 15:02:35 +0000

    Added missing includes

--- mailbox/date.c
+++ mailbox/date.c
@@ -15,6 +15,12 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <mailutils/mutil.h>
 
 #define SECS_PER_DAY 86400
