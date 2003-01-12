84277968048a81587e84166daa6f76e9d00c70b6
Jordi Mallach <jordi@gnu.org>
2003-01-12 16:47:20 +0000

    Update year in copyright notice.

--- mail/mail.c
+++ mail/mail.c
@@ -1,5 +1,5 @@
 /* GNU Mailutils -- a suite of utilities for electronic mail
-   Copyright (C) 1999, 2000, 2001, 2002 Free Software Foundation, Inc.
+   Copyright (C) 1999, 2000, 2001, 2002, 2003 Free Software Foundation, Inc.
 
    GNU Mailutils is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -503,7 +503,7 @@ mail_warranty (int argc, char **argv)
   (void)argc; (void)argv;
 
   fputs (_("GNU Mailutils -- a suite of utilities for electronic mail\n"
-           "Copyright (C) 1999, 2000, 2001, 2002 Free Software Foundation, Inc.\n\n"),
+           "Copyright (C) 1999, 2000, 2001, 2002, 2003 Free Software Foundation, Inc.\n\n"),
            ofile);
   fputs (
   _("   GNU Mailutils is free software; you can redistribute it and/or modify\n"
