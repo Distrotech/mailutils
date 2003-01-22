6639ff69434318492083b9d71f74839f56637fb6
Sergey Poznyakoff <gray@gnu.org.ua>
2003-01-22 20:21:47 +0000

    (gsasl_stream_create): Changed declaration. (mu_gsasl_init_argp): New function. (gsasl_cram_md5_pwd): New global.

--- include/mailutils/gsasl.h
+++ include/mailutils/gsasl.h
@@ -1,4 +1,29 @@
+/* GNU Mailutils -- a suite of utilities for electronic mail
+   Copyright (C) 2003 Free Software Foundation, Inc.
 
-int gsasl_stream_create __P((stream_t *stream, stream_t s,
+   GNU Mailutils is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Library Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   GNU Mailutils is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Library General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with GNU Mailutils; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA  */
+
+#ifndef _MAILUTILS_GSASL_H
+#define _MAILUTILS_GSASL_H
+
+int gsasl_stream_create __P((stream_t *stream, int fd,
 			     Gsasl_session_ctx *ctx,
 			     int flags));
+
+void mu_gsasl_init_argp __P((void));
+
+extern char *gsasl_cram_md5_pwd;
+
+#endif
