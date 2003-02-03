33ba9b3c71438b709129b7c7cb7760b571a0dbe6
Sergey Poznyakoff <gray@gnu.org.ua>
2003-02-03 11:28:04 +0000

    (struct _stream): Changed declaration of _strerror method.

--- mailbox/include/stream0.h
+++ mailbox/include/stream0.h
@@ -62,7 +62,7 @@ struct _stream
   int (*_size)     __P ((stream_t, off_t *));
   int (*_flush)    __P ((stream_t));
   int (*_setbufsiz)__P ((stream_t, size_t));
-  int (*_strerror) __P ((stream_t, const char **));
+  int (*_strerror) __P ((stream_t, char **));
 };
 
 #ifdef __cplusplus
