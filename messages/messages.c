c3950a84b516798e28a119ec960bc0a4e95ce865
Wojciech Polak <polak@gnu.org>
2003-02-09 22:44:31 +0000

    Fixed copyright.

--- messages/messages.c
+++ messages/messages.c
@@ -2,14 +2,14 @@
    Copyright (C) 1999, 2000, 2001, 2002 Free Software Foundation, Inc.
 
    GNU Mailutils is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Library Public License as published by
+   it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2, or (at your option)
    any later version.
 
    GNU Mailutils is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU Library General Public License for more details.
+   GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
    along with GNU Mailutils; if not, write to the Free Software
