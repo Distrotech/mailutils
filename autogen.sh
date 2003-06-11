5cdf62b95db54f1db42b73a620977dd8d3ffaa13
Sergey Poznyakoff <gray@gnu.org.ua>
2003-06-11 09:59:37 +0000

    A hack to force automake to install COPYING file.

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# This trick forces automake to install COPYING
+trap "mv $$.COPYING.LIB COPYING.LIB" 1 2 3 15 
+mv COPYING.LIB $$.COPYING.LIB
 autoreconf -f -i -s
+mv $$.COPYING.LIB COPYING.LIB
