5ccb23a8f54176e15fe1e76ea99dbc6f46032ec6
Sergey Poznyakoff <gray@gnu.org.ua>
2003-09-25 14:08:39 +0000

    Added missing includes.

--- auth/lbuf.c
+++ auth/lbuf.c
@@ -21,6 +21,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/types.h>
 #include <lbuf.h>
 
 struct _line_buffer {
--- auth/lbuf.h
+++ auth/lbuf.h
@@ -15,6 +15,8 @@
    License along with this library; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA  */
 
+#include <mailutils/types.h>
+
 struct _line_buffer;
 
 int _auth_lb_create __P((struct _line_buffer **s));
