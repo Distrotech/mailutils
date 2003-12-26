3521def033d76a41fcfc994fdd28a0287c6ba0ef
uid65697 <uid65697>
2003-12-26 16:25:21 +0000

    Updated

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-# This trick forces automake to install COPYING
-trap "mv $$.COPYING.LIB COPYING.LIB" 1 2 3 15 
-mv COPYING.LIB $$.COPYING.LIB
 autoreconf -f -i -s
-mv $$.COPYING.LIB COPYING.LIB
