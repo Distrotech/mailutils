c9cc6e1c044634d883e06edb34ed50cc5ab16d98
Sergey Poznyakoff <gray@gnu.org.ua>
2004-01-08 16:28:12 +0000

    Added to the repository

---
+++ mailbox/maildir/folder.c
@@ -0,0 +1,55 @@
+/* GNU Mailutils -- a suite of utilities for electronic mail
+   Copyright (C) 2004 Free Software Foundation, Inc.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA  */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#ifdef ENABLE_MAILDIR
+
+#include <errno.h>
+
+#include <folder0.h>
+#include <registrar0.h>
+
+static struct _record _maildir_record =
+{
+  MU_MAILDIR_SCHEME,
+  _url_maildir_init, /* Url init.  */
+  _mailbox_maildir_init, /* Mailbox init.  */
+  NULL, /* Mailer init.  */
+  _folder_maildir_init, /* Folder init.  */
+  NULL, /* back pointer.  */
+  NULL, /* _is_scheme method.  */
+  NULL, /* _get_url method.  */
+  NULL, /* _get_mailbox method.  */
+  NULL, /* _get_mailer method.  */
+  NULL  /* _get_folder method.  */
+};
+record_t maildir_record = &_maildir_record;
+
+int
+_folder_maildir_init (folder_t folder ARG_UNUSED)
+{
+  return 0;
+}
+
+#else
+#include <stdio.h>
+#include <registrar0.h>
+record_t maildir_record = NULL;
+#endif
