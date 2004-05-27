303d20fb7bf239f984b5125f492ffaebce43d73a
Sergey Poznyakoff <gray@gnu.org.ua>
2004-05-27 19:07:52 +0000

    New implementation of RFC 2047 "Q" filter. Reorganized all functions.

--- mailbox/filter_trans.c
+++ mailbox/filter_trans.c
@@ -36,61 +36,8 @@ First Draft: Dave Inglis.
 #define MU_TRANS_ENCODE		2
 #define MU_TRANS_BSIZE		2048
 
-static int qp_init __P ((filter_t));
-static int base64_init __P ((filter_t));
-
-static struct _filter_record _qp_filter =
-{
-  "quoted-printable",
-  qp_init,
-  NULL,
-  NULL,
-  NULL
-};
-
-static struct _filter_record _base64_filter =
-{
-  "base64",
-  base64_init,
-  NULL,
-  NULL,
-  NULL
-};
-
-static struct _filter_record _bit8_filter =
-{
-  "8bit",
-  NULL,
-  NULL,
-  NULL,
-  NULL
-};
-
-static struct _filter_record _bit7_filter =
-{
-  "7bit",
-  NULL,
-  NULL,
-  NULL,
-  NULL
-};
-
-static struct _filter_record _binary_filter =
-{
-  "binary",
-  NULL,
-  NULL,
-  NULL,
-  NULL
-};
-
-/* Export.  */
-filter_record_t qp_filter = &_qp_filter;
-filter_record_t base64_filter = &_base64_filter;
-filter_record_t binary_filter = &_binary_filter;
-filter_record_t bit8_filter = &_bit8_filter;
-filter_record_t bit7_filter = &_bit7_filter;
-
+
+/* General-purpose implementation */
 struct _trans_stream
 {
   int t_offset; /* Orignal stream offset.  */
@@ -111,16 +58,6 @@ struct _trans_stream
 			  size_t osize, size_t *nbytes, int *line_len));
 };
 
-static int base64_decode __P ((const char *iptr, size_t isize, char *optr,
-				size_t osize, size_t *nbytes, int *line_len));
-static int base64_encode __P ((const char *iptr, size_t isize, char *optr,
-				size_t osize, size_t *nbytes, int *line_len));
-
-static int qp_decode __P ((const char *iptr, size_t isize, char *optr,
-			    size_t osize, size_t *nbytes, int *line_len));
-static int qp_encode __P ((const char *iptr, size_t isize, char *optr,
-			    size_t osize, size_t *nbytes, int *line_len));
-
 static void
 trans_destroy (filter_t filter)
 {
@@ -132,7 +69,7 @@ trans_destroy (filter_t filter)
 
 static int
 trans_read (filter_t filter, char *optr, size_t osize, off_t offset,
-	     size_t *n_bytes)
+	    size_t *n_bytes)
 {
   struct _trans_stream *ts = filter->data;
   size_t obytes, wbytes = 0;
@@ -212,139 +149,7 @@ trans_read (filter_t filter, char *optr, size_t osize, off_t offset,
   return ret;
 }
 
-#if 0
-static int
-trans_write (stream_t stream, const char *iptr, size_t isize, off_t offset,
-	      size_t *nbytes)
-{
-  struct _trans_stream *ts = stream_get_owner (stream);
-  return stream_write (ts->stream, iptr, isize, offset, nbytes);
-}
-#endif
-
-
-/*------------------------------------------------------
- * base64 encode/decode
- *----------------------------------------------------*/
-
-static int
-b64_input (char c)
-{
-  const char table[64] =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-  int i;
-
-  for (i = 0; i < 64; i++)
-    {
-      if (table[i] == c)
-	return i;
-    }
-  return -1;
-}
-
-static int
-base64_init (filter_t filter)
-{
-  struct _trans_stream *ts;
-  ts = calloc (sizeof (*ts), 1);
-  if (ts == NULL)
-    return ENOMEM;
-
-  ts->min_size = 4;
-  ts->s_buf = calloc (ts->min_size, 1);
-  if (ts->s_buf == NULL)
-    {
-      free (ts);
-      return ENOMEM;
-    }
-  ts->transcoder = (filter->type == MU_FILTER_DECODE) ? base64_decode : base64_encode;
-
-  filter->_read = trans_read;
-  filter->_destroy = trans_destroy;
-  filter->data = ts;
-  return 0;
-}
-
-static int
-base64_decode (const char *iptr, size_t isize, char *optr, size_t osize,
-	       size_t *nbytes, int *line_len ARG_UNUSED)
-{
-  int i = 0, tmp = 0, pad = 0;
-  size_t consumed = 0;
-  unsigned char data[4];
-
-  *nbytes = 0;
-  while (consumed < isize && (*nbytes)+3 < osize)
-    {
-      while (( i < 4 ) && (consumed < isize))
-	{
-	  tmp = b64_input (*iptr++);
-	  consumed++;
-	  if (tmp != -1)
-	    data[i++] = tmp;
-	  else if (*(iptr-1) == '=')
-	    {
-	      data[i++] = '\0';
-	      pad++;
-	    }
-	}
-
-      /* I have a entire block of data 32 bits get the output data.  */
-      if (i == 4)
-	{
-	  *optr++ = (data[0] << 2) | ((data[1] & 0x30) >> 4);
-	  *optr++ = ((data[1] & 0xf) << 4) | ((data[2] & 0x3c) >> 2);
-	  *optr++ = ((data[2] & 0x3) << 6) | data[3];
-	  (*nbytes) += 3 - pad;
-	}
-      else
-	{
-	  /* I did not get all the data.  */
-	  consumed -= i;
-	  return consumed;
-	}
-      i = 0;
-    }
-  return consumed;
-}
-
-#define BASE64_LINE_MAX 	77
-static int
-base64_encode (const char *iptr, size_t isize, char *optr, size_t osize,
-	       size_t *nbytes, int *line_len)
-{
-  size_t consumed = 0;
-  int pad = 0;
-  const char *b64 =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-  const unsigned char* ptr = (const unsigned char*) iptr;
-	
-  *nbytes = 0;
-  if (isize <= 3)
-    pad = 1;
-  while (((consumed + 3) <= isize && (*nbytes + 4) <= osize) || pad)
-    {
-      if (*line_len == 76)
-	{
-	  *optr++ = '\n';
-	  (*nbytes)++;
-	  (*line_len) = 0;
-	  if ((*nbytes + 4) > osize)
-	    return consumed;
-	}
-      *optr++ = b64[ptr[0] >> 2];
-      *optr++ = b64[((ptr[0] << 4) + (--isize ? (ptr[1] >> 4): 0)) & 0x3f];
-      *optr++ = isize ? b64[((ptr[1] << 2) + (--isize ? (ptr[2] >> 6) : 0 )) & 0x3f] : '=';
-      *optr++ = isize ? b64[ptr[2] & 0x3f] : '=';
-      ptr += 3;
-      consumed += 3;
-      (*nbytes) += 4;
-      (*line_len) +=4;
-      pad = 0;
-    }
-  return consumed;
-}
-
+
 /*------------------------------------------------------
  * quoted-printable decoder/encoder
  *------------------------------------------------------*/
@@ -352,29 +157,6 @@ static const char _hexdigits[] = "0123456789ABCDEF";
 
 #define QP_LINE_MAX	76
 #define ISWS(c) ((c)==' ' || (c)=='\t')
-
-static int
-qp_init (filter_t filter)
-{
-  struct _trans_stream *ts;
-  ts = calloc (sizeof (*ts), 1);
-  if (ts == NULL)
-    return ENOMEM;
-
-  ts->min_size = QP_LINE_MAX;
-  ts->s_buf = calloc (ts->min_size, 1);
-  if (ts->s_buf == NULL)
-    {
-      free (ts);
-      return ENOMEM;
-    }
-  ts->transcoder = (filter->type == MU_FILTER_DECODE) ? qp_decode : qp_encode;
-
-  filter->_read = trans_read;
-  filter->_destroy = trans_destroy;
-  filter->data = ts;
-  return 0;
-}
 
 static int
 qp_decode (const char *iptr, size_t isize, char *optr, size_t osize,
@@ -471,12 +253,6 @@ qp_decode (const char *iptr, size_t isize, char *optr, size_t osize,
 		  (*nbytes)++;
 		  consumed += 2;
 		}
-	    }
-	  else if (c == '_')
-	    {
-	      *optr++ = ' ';
-	      (*nbytes)++;
-	      consumed++;
 	    }
 	  else
 	    {
@@ -585,3 +361,393 @@ qp_encode (const char *iptr, size_t isize, char *optr, size_t osize,
   (*nbytes) -= wscount;
   return consumed - wscount;
 }
+
+static int
+qp_init (filter_t filter)
+{
+  struct _trans_stream *ts;
+  ts = calloc (sizeof (*ts), 1);
+  if (ts == NULL)
+    return ENOMEM;
+
+  ts->min_size = QP_LINE_MAX;
+  ts->s_buf = calloc (ts->min_size, 1);
+  if (ts->s_buf == NULL)
+    {
+      free (ts);
+      return ENOMEM;
+    }
+  ts->transcoder = (filter->type == MU_FILTER_DECODE) ? qp_decode : qp_encode;
+
+  filter->_read = trans_read;
+  filter->_destroy = trans_destroy;
+  filter->data = ts;
+  return 0;
+}
+
+static struct _filter_record _qp_filter =
+{
+  "quoted-printable",
+  qp_init,
+  NULL,
+  NULL,
+  NULL
+};
+
+
+/*------------------------------------------------------
+ * base64 encode/decode
+ *----------------------------------------------------*/
+
+static int
+b64_input (char c)
+{
+  const char table[64] =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  int i;
+
+  for (i = 0; i < 64; i++)
+    {
+      if (table[i] == c)
+	return i;
+    }
+  return -1;
+}
+
+static int
+base64_decode (const char *iptr, size_t isize, char *optr, size_t osize,
+	       size_t *nbytes, int *line_len ARG_UNUSED)
+{
+  int i = 0, tmp = 0, pad = 0;
+  size_t consumed = 0;
+  unsigned char data[4];
+
+  *nbytes = 0;
+  while (consumed < isize && (*nbytes)+3 < osize)
+    {
+      while (( i < 4 ) && (consumed < isize))
+	{
+	  tmp = b64_input (*iptr++);
+	  consumed++;
+	  if (tmp != -1)
+	    data[i++] = tmp;
+	  else if (*(iptr-1) == '=')
+	    {
+	      data[i++] = '\0';
+	      pad++;
+	    }
+	}
+
+      /* I have a entire block of data 32 bits get the output data.  */
+      if (i == 4)
+	{
+	  *optr++ = (data[0] << 2) | ((data[1] & 0x30) >> 4);
+	  *optr++ = ((data[1] & 0xf) << 4) | ((data[2] & 0x3c) >> 2);
+	  *optr++ = ((data[2] & 0x3) << 6) | data[3];
+	  (*nbytes) += 3 - pad;
+	}
+      else
+	{
+	  /* I did not get all the data.  */
+	  consumed -= i;
+	  return consumed;
+	}
+      i = 0;
+    }
+  return consumed;
+}
+
+#define BASE64_LINE_MAX 	77
+static int
+base64_encode (const char *iptr, size_t isize, char *optr, size_t osize,
+	       size_t *nbytes, int *line_len)
+{
+  size_t consumed = 0;
+  int pad = 0;
+  const char *b64 =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  const unsigned char* ptr = (const unsigned char*) iptr;
+	
+  *nbytes = 0;
+  if (isize <= 3)
+    pad = 1;
+  while (((consumed + 3) <= isize && (*nbytes + 4) <= osize) || pad)
+    {
+      if (*line_len == 76)
+	{
+	  *optr++ = '\n';
+	  (*nbytes)++;
+	  (*line_len) = 0;
+	  if ((*nbytes + 4) > osize)
+	    return consumed;
+	}
+      *optr++ = b64[ptr[0] >> 2];
+      *optr++ = b64[((ptr[0] << 4) + (--isize ? (ptr[1] >> 4): 0)) & 0x3f];
+      *optr++ = isize ? b64[((ptr[1] << 2) + (--isize ? (ptr[2] >> 6) : 0 )) & 0x3f] : '=';
+      *optr++ = isize ? b64[ptr[2] & 0x3f] : '=';
+      ptr += 3;
+      consumed += 3;
+      (*nbytes) += 4;
+      (*line_len) +=4;
+      pad = 0;
+    }
+  return consumed;
+}
+
+static int
+base64_init (filter_t filter)
+{
+  struct _trans_stream *ts;
+  ts = calloc (sizeof (*ts), 1);
+  if (ts == NULL)
+    return ENOMEM;
+
+  ts->min_size = 4;
+  ts->s_buf = calloc (ts->min_size, 1);
+  if (ts->s_buf == NULL)
+    {
+      free (ts);
+      return ENOMEM;
+    }
+  ts->transcoder = (filter->type == MU_FILTER_DECODE) ? base64_decode : base64_encode;
+
+  filter->_read = trans_read;
+  filter->_destroy = trans_destroy;
+  filter->data = ts;
+  return 0;
+}
+
+static struct _filter_record _base64_filter =
+{
+  "base64",
+  base64_init,
+  NULL,
+  NULL,
+  NULL
+};
+
+
+
+/* RFC 2047 "Q" Encoding */
+static int
+Q_decode (const char *iptr, size_t isize, char *optr, size_t osize,
+	   size_t *nbytes, int *line_len ARG_UNUSED)
+{
+  char c;
+  size_t consumed = 0;
+  
+  *nbytes = 0;
+  while (consumed < isize && *nbytes < osize)
+    {
+      c = *iptr++;
+
+      if (c == '=')
+	{
+	  /* There must be 2 more characters before I consume this.  */
+	  if (consumed + 2 >= isize)
+	    break;
+	  else
+	    {
+	      /* you get =XX where XX are hex characters.  */
+	      char chr[3];
+	      int  new_c;
+	      
+	      chr[2] = 0;
+	      chr[0] = *iptr++;
+	      /* Ignore LF.  */
+	      if (chr[0] != '\n')
+		{
+		  chr[1] = *iptr++;
+		  new_c = strtoul (chr, NULL, 16);
+		  *optr++ = new_c;
+		  (*nbytes)++;
+		  consumed += 3;
+		}
+	      else
+		consumed += 2;
+	    }
+	}
+      /* CR character.  */
+      else if (c == '\r')
+	{
+	  /* There must be at least 1 more character before
+	     I consume this.  */
+	  if (consumed + 1 >= isize)
+	    break;
+	  else
+	    {
+	      iptr++; /* Skip the CR character.  */
+	      *optr++ = '\n';
+	      (*nbytes)++;
+	      consumed += 2;
+	    }
+	}
+      else if (c == '_')
+	{
+	  *optr++ = ' ';
+	  (*nbytes)++;
+	  consumed++;
+	}
+      else
+	{
+	  *optr++ = c;
+	  (*nbytes)++;
+	  consumed++;
+	}
+    }	  
+  return consumed;
+}
+
+static int
+Q_printable_char_p (unsigned c)
+{
+  switch (c)
+    {
+      /* FIXME: This is not quite so. Says RFC 2047:
+	 
+      (3) 8-bit values which correspond to printable ASCII characters other
+      than "=", "?", and "_" (underscore), MAY be represented as those
+      characters.  (But see section 5 for restrictions.)  In
+      particular, SPACE and TAB MUST NOT be represented as themselves
+      within encoded words. (see Page 6)*/
+					
+    case '=':
+    case '?':
+    case '_':
+    case ' ':
+      return 0;
+    default:
+      return 1;
+    }
+}
+
+static int
+Q_encode (const char *iptr, size_t isize, char *optr, size_t osize,
+	   size_t *nbytes, int *line_len)
+{
+  unsigned int c;
+  size_t consumed = 0;
+
+  *nbytes = 0;
+
+  while (consumed < isize)
+    {
+      c = *(unsigned char*) iptr;
+      if (Q_printable_char_p (c))
+	{
+	  /* a non-quoted character uses up one byte */
+	  if (*nbytes + 1 > osize) 
+	    break;
+	  
+	  *optr++ = c;
+	  (*nbytes)++;
+	  (*line_len)++;
+	  
+	  iptr++;
+	  consumed++;
+	}
+      else if (c == '_')
+	{
+	  *optr++ = ' ';
+	  (*nbytes)++;
+	  (*line_len)++;
+	  iptr++;
+	  consumed++;
+	}
+      else 
+	{
+	  /* a quoted character uses up three bytes */
+	  if ((*nbytes) + 3 > osize) 
+	    break;
+
+	  *optr++ = '=';
+	  *optr++ = _hexdigits[(c >> 4) & 0xf];
+	  *optr++ = _hexdigits[c & 0xf];
+
+	  (*nbytes) += 3;
+	  (*line_len) += 3;
+
+	  /* we've actuall used up one byte of input */
+	  iptr++;
+	  consumed++;
+	}
+    }
+  return consumed;
+}
+
+static int
+Q_init (filter_t filter)
+{
+  struct _trans_stream *ts;
+  ts = calloc (sizeof (*ts), 1);
+  if (ts == NULL)
+    return ENOMEM;
+
+  ts->min_size = QP_LINE_MAX;
+  ts->s_buf = calloc (ts->min_size, 1);
+  if (ts->s_buf == NULL)
+    {
+      free (ts);
+      return ENOMEM;
+    }
+  ts->transcoder = (filter->type == MU_FILTER_DECODE) ? Q_decode : Q_encode;
+
+  filter->_read = trans_read;
+  filter->_destroy = trans_destroy;
+  filter->data = ts;
+  return 0;
+}
+
+static struct _filter_record _Q_filter =
+{
+  "Q",
+  Q_init,
+  NULL,
+  NULL,
+  NULL
+};
+
+
+/* Pass-through encodings */
+
+static struct _filter_record _bit8_filter =
+{
+  "8bit",
+  NULL,
+  NULL,
+  NULL,
+  NULL
+};
+
+static struct _filter_record _bit7_filter =
+{
+  "7bit",
+  NULL,
+  NULL,
+  NULL,
+  NULL
+};
+
+static struct _filter_record _binary_filter =
+{
+  "binary",
+  NULL,
+  NULL,
+  NULL,
+  NULL
+};
+
+
+
+/* Export.  */
+filter_record_t qp_filter = &_qp_filter;
+filter_record_t base64_filter = &_base64_filter;
+filter_record_t binary_filter = &_binary_filter;
+filter_record_t bit8_filter = &_bit8_filter;
+filter_record_t bit7_filter = &_bit7_filter;
+filter_record_t rfc_2047_Q_filter = &_Q_filter;
+
+
+
+
+
