e0f85d375556ebe4e693d8632e6078a2ddca0a57
Sergey Poznyakoff <gray@gnu.org.ua>
2004-07-01 09:05:29 +0000

    (struct _iterator.getitem): Changed declaration

--- mailbox/include/iterator0.h
+++ mailbox/include/iterator0.h
@@ -1,5 +1,5 @@
 /* GNU Mailutils -- a suite of utilities for electronic mail
-   Copyright (C) 1999, 2000 Free Software Foundation, Inc.
+   Copyright (C) 1999, 2000, 2004 Free Software Foundation, Inc.
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
@@ -38,7 +38,7 @@ struct _iterator
   int (*destroy) (iterator_t itr, void *owner);
   int (*first) (void *owner);
   int (*next) (void *owner);
-  int (*getitem) (void *owner, void **pret);
+  int (*getitem) (void *owner, void * const *pret);
   int (*curitem_p) (void *owner, void *item);
   int (*finished_p) (void *owner);
 };
