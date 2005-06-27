7061bd30c48e97236a36e58a76c7fc7de803a919
Sergey Poznyakoff <gray@gnu.org.ua>
2005-06-27 11:07:53 +0000

    (argcv_unescape_char): Renamed to argcv_unquote_char (argcv_escape_char): Renamed to argcv_quote_char. (argcv_quoted_length,argcv_quote_copy): New functions

--- include/mailutils/argcv.h
+++ include/mailutils/argcv.h
@@ -1,5 +1,5 @@
 /* GNU Mailutils -- a suite of utilities for electronic mail
-   Copyright (C) 1999, 2000, 2001 Free Software Foundation, Inc.
+   Copyright (C) 1999, 2000, 2001, 2005 Free Software Foundation, Inc.
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
@@ -41,9 +41,12 @@ extern int argcv_get    __P ((const char *command, const char *delim,
 			      int *argc, char ***argv));
 extern int argcv_string __P ((int argc, char **argv, char **string));
 extern int argcv_free   __P ((int argc, char **argv));
-extern int argcv_unescape_char __P((int c));
-extern int argcv_escape_char __P((int c));
-
+extern int argcv_unquote_char __P((int c));
+extern int argcv_quote_char   __P((int c));
+extern size_t argcv_quoted_length __P((const char *str, int *quote));
+extern void argcv_unquote_copy __P((char *dst, const char *src, size_t n));
+extern void argcv_quote_copy __P((char *dst, const char *src));
+  
 #ifdef __cplusplus
 }
 #endif
