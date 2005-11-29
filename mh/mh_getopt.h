b111a6cc76b771f689304d076f1bfee02c2b7230
Sergey Poznyakoff <gray@gnu.org.ua>
2005-11-29 17:41:25 +0000

    (enum mh_arg): New declaration, instead of multiple defines.

--- mh/mh_getopt.h
+++ mh/mh_getopt.h
@@ -39,143 +39,146 @@ struct mh_argp_data
   char *doc;
 };
 
-#define ARG_ADD		        257
-#define ARG_AFTER		258
-#define ARG_ALIAS		259
-#define ARG_ALL		        260
-#define ARG_AND		        261
-#define ARG_ANNOTATE		262
-#define ARG_AUDIT		263
-#define ARG_AUTO		264
-#define ARG_BEFORE		265
-#define ARG_BELL		266
-#define ARG_BUILD		267
-#define ARG_CC		        268
-#define ARG_CFLAGS		269
-#define ARG_CHANGECUR		270
-#define ARG_CHECK		271
-#define ARG_CLEAR		272
-#define ARG_COMPAT		273
-#define ARG_COMPONENT		274
-#define ARG_COMPOSE		275
-#define ARG_CREATE		276
-#define ARG_DATE		277
-#define ARG_DATEFIELD		278
-#define ARG_DEBUG		279
-#define ARG_DELETE		280
-#define ARG_DRAFT		281
-#define ARG_DRAFTFOLDER		282
-#define ARG_DRAFTMESSAGE	283
-#define ARG_DRY_RUN		284
-#define ARG_DUMP		285
-#define ARG_EDITOR		286
-#define ARG_FAST		287
-#define ARG_FCC		        288
-#define ARG_FILE		289
-#define ARG_FILTER		290
-#define ARG_FOLDER		291
-#define ARG_FORM		292
-#define ARG_FORMAT		293
-#define ARG_FORWARD		294
-#define ARG_FROM		295
-#define ARG_GROUP               296 
-#define ARG_HEADER		297
-#define ARG_INPLACE		298
-#define ARG_INTERACTIVE		299
-#define ARG_LBRACE		300
-#define ARG_LENGTH		301
-#define ARG_LICENSE		302
-#define ARG_LIMIT		303
-#define ARG_LINK		304
-#define ARG_LIST		305
-#define ARG_MIME		306
-#define ARG_MOREPROC		307
-#define ARG_MSGID		308
-#define ARG_NOALIAS             309
-#define ARG_NOAUDIT		310
-#define ARG_NOAUTO		311
-#define ARG_NOBELL		312
-#define ARG_NOCC		313
-#define ARG_NOCHANGECUR		314
-#define ARG_NOCHECK		315
-#define ARG_NOCLEAR		316
-#define ARG_NOCOMPOSE		317
-#define ARG_NOCREATE		318
-#define ARG_NODATE		319
-#define ARG_NODATEFIELD		320
-#define ARG_NODRAFTFOLDER	321
-#define ARG_NOEDIT		322
-#define ARG_NOFAST		323
-#define ARG_NOFILTER		324
-#define ARG_NOFORMAT		325
-#define ARG_NOFORWARD		326
-#define ARG_NOHEADER		327
-#define ARG_NOHEADERS		328
-#define ARG_NOINTERACTIVE       329 
-#define ARG_NOINPLACE		330 
-#define ARG_NOLIMIT		331 
-#define ARG_NOLIST		332 
-#define ARG_NOMIME		333 
-#define ARG_NOMOREPROC		334 
-#define ARG_NOMSGID		335 
-#define ARG_NOPAUSE		336 
-#define ARG_NOPUBLIC		337 
-#define ARG_NOPUSH		338 
-#define ARG_NOREALSIZE		339 
-#define ARG_NORECURSIVE         340 
-#define ARG_NOREVERSE		341
-#define ARG_NORMALIZE           342
-#define ARG_NOSERIALONLY	343 
-#define ARG_NOSHOW		344 
-#define ARG_NOSTORE		345 
-#define ARG_NOT		        346 
-#define ARG_NOTEXTFIELD		347 
-#define ARG_NOTOTAL		348 
-#define ARG_NOTRUNCATE		349 
-#define ARG_NOUSE		350 
-#define ARG_NOVERBOSE		351 
-#define ARG_NOWATCH		352 
-#define ARG_NOWHATNOWPROC	353 
-#define ARG_NOZERO		354 
-#define ARG_NUMFIELD		355 
-#define ARG_OR		        356 
-#define ARG_PACK                357 
-#define ARG_PART		358 
-#define ARG_PATTERN		359 
-#define ARG_PAUSE		360 
-#define ARG_POP		        361 
-#define ARG_PRESERVE		362 
-#define ARG_PRINT		363 
-#define ARG_PROMPT		364 
-#define ARG_PUBLIC		365 
-#define ARG_PUSH		366 
-#define ARG_QUERY		367 
-#define ARG_QUIET		368 
-#define ARG_RBRACE		369 
-#define ARG_REALSIZE		370 
-#define ARG_RECURSIVE		371 
-#define ARG_REORDER		372 
-#define ARG_REVERSE		373 
-#define ARG_SEQUENCE		374 
-#define ARG_SERIALONLY		375 
-#define ARG_SHOW		376 
-#define ARG_SOURCE		377 
-#define ARG_SPLIT		378 
-#define ARG_STORE		379 
-#define ARG_SUBJECT		380 
-#define ARG_TEXT		381 
-#define ARG_TEXTFIELD		382 
-#define ARG_TO		        383 
-#define ARG_TOTAL		384 
-#define ARG_TRUNCATE		385 
-#define ARG_TYPE		386 
-#define ARG_USE		        387 
-#define ARG_USER                388 
-#define ARG_VERBOSE		389 
-#define ARG_WATCH		390 
-#define ARG_WHATNOWPROC		391 
-#define ARG_WIDTH		392 
-#define ARG_ZERO		393
+enum mh_arg {
+  ARG_ADD = 256,		
+  ARG_AFTER,		
+  ARG_ALIAS,		
+  ARG_ALL,		
+  ARG_AND,		
+  ARG_ANNOTATE,		
+  ARG_AUDIT,		
+  ARG_AUTO,		
+  ARG_BEFORE,		
+  ARG_BELL,		
+  ARG_BUILD,		
+  ARG_CC,	        
+  ARG_CFLAGS,		
+  ARG_CHANGECUR,		
+  ARG_CHECK,		
+  ARG_CLEAR,		
+  ARG_COMPAT,		
+  ARG_COMPONENT,		
+  ARG_COMPOSE,		
+  ARG_CREATE,		
+  ARG_DATE,		
+  ARG_DATEFIELD,		
+  ARG_DEBUG,		
+  ARG_DELETE,		
+  ARG_DRAFT,		
+  ARG_DRAFTFOLDER,	
+  ARG_DRAFTMESSAGE,	
+  ARG_DRY_RUN,		
+  ARG_DUMP,		
+  ARG_EDITOR,		
+  ARG_FAST,		
+  ARG_FCC,		
+  ARG_FILE,		
+  ARG_FILTER,		
+  ARG_FOLDER,		
+  ARG_FORM,		
+  ARG_FORMAT,		
+  ARG_FORWARD,		
+  ARG_FROM,		
+  ARG_GROUP,              
+  ARG_HEADER,		
+  ARG_INPLACE,		
+  ARG_INTERACTIVE,	
+  ARG_LBRACE,		
+  ARG_LENGTH,		
+  ARG_LICENSE,		
+  ARG_LIMIT,		
+  ARG_LINK,		
+  ARG_LIST,		
+  ARG_MIME,		
+  ARG_MOREPROC,		
+  ARG_MSGID,		
+  ARG_NOALIAS,            
+  ARG_NOAUDIT,		
+  ARG_NOAUTO,		
+  ARG_NOBELL,		
+  ARG_NOCC,		
+  ARG_NOCHANGECUR,	
+  ARG_NOCHECK,		
+  ARG_NOCLEAR,		
+  ARG_NOCOMPOSE,		
+  ARG_NOCREATE,		
+  ARG_NODATE,		
+  ARG_NODATEFIELD,	
+  ARG_NODRAFTFOLDER,	
+  ARG_NOEDIT,		
+  ARG_NOFAST,		
+  ARG_NOFILTER,		
+  ARG_NOFORMAT,		
+  ARG_NOFORWARD,		
+  ARG_NOHEADER,		
+  ARG_NOHEADERS,		
+  ARG_NOINTERACTIVE,      
+  ARG_NOINPLACE,		
+  ARG_NOLIMIT,		
+  ARG_NOLIST,		
+  ARG_NOMIME,		
+  ARG_NOMOREPROC,		
+  ARG_NOMSGID,		
+  ARG_NOPAUSE,		
+  ARG_NOPUBLIC,		
+  ARG_NOPUSH,		
+  ARG_NOQUIET,            
+  ARG_NOREALSIZE,		
+  ARG_NORECURSIVE,        
+  ARG_NOREVERSE,	
+  ARG_NORMALIZE,          
+  ARG_NOSERIALONLY,	
+  ARG_NOSHOW,		
+  ARG_NOSTORE,		
+  ARG_NOT,		
+  ARG_NOTEXTFIELD,	
+  ARG_NOTOTAL,		
+  ARG_NOTRUNCATE,		
+  ARG_NOUSE,		
+  ARG_NOVERBOSE,		
+  ARG_NOWATCH,		
+  ARG_NOWHATNOWPROC,	
+  ARG_NOZERO,		
+  ARG_NUMFIELD,		
+  ARG_OR,		        
+  ARG_PACK,               
+  ARG_PART,		
+  ARG_PATTERN,		
+  ARG_PAUSE,		
+  ARG_POP,		
+  ARG_PRESERVE,		
+  ARG_PRINT,		
+  ARG_PROMPT,		
+  ARG_PUBLIC,		
+  ARG_PUSH,		
+  ARG_QUERY,		
+  ARG_QUIET,		
+  ARG_RBRACE,		
+  ARG_REALSIZE,		
+  ARG_RECURSIVE,		
+  ARG_REORDER,		
+  ARG_REVERSE,		
+  ARG_SEQUENCE,		
+  ARG_SERIALONLY,		
+  ARG_SHOW,		
+  ARG_SOURCE,		
+  ARG_SPLIT,		
+  ARG_STORE,		
+  ARG_SUBJECT,		
+  ARG_TEXT,		
+  ARG_TEXTFIELD,		
+  ARG_TO,		        
+  ARG_TOTAL,		
+  ARG_TRUNCATE,		
+  ARG_TYPE,		
+  ARG_USE,		
+  ARG_USER,               
+  ARG_VERBOSE,		
+  ARG_WATCH,		
+  ARG_WHATNOWPROC,	
+  ARG_WIDTH,	
+  ARG_ZERO
+};
 
 void mh_argv_preproc (int argc, char **argv, struct mh_argp_data *data);
 int mh_getopt (int argc, char **argv, struct mh_option *mh_opt, const char *doc);
