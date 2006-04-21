1413f6d14317eba925816e9602c7d4139c843ca0
Sergey Poznyakoff <gray@gnu.org.ua>
2006-04-21 22:05:42 +0000

    Include mailutils/errno.h

--- libmu_scm/mu_scm.h
+++ libmu_scm/mu_scm.h
@@ -1,5 +1,5 @@
 /* GNU Mailutils -- a suite of utilities for electronic mail
-   Copyright (C) 1999, 2000, 2001, 2002 Free Software Foundation, Inc.
+   Copyright (C) 1999, 2000, 2001, 2002, 2006 Free Software Foundation, Inc.
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
@@ -33,6 +33,7 @@
 #include <mailutils/body.h>
 #include <mailutils/registrar.h>
 #include <mailutils/error.h>
+#include <mailutils/errno.h>
 #include <mailutils/address.h>
 #include <mailutils/registrar.h>
 #include <mailutils/mutil.h>
