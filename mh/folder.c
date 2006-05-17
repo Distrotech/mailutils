0baee414bcfa5dac5cb309e86800ada84f0eaefa
Sergey Poznyakoff <gray@gnu.org.ua>
2006-05-17 09:00:15 +0000

    Update

--- mh/folder.c
+++ mh/folder.c
@@ -1,6 +1,6 @@
 /* GNU Mailutils -- a suite of utilities for electronic mail
    Copyright (C) 1999, 2000, 2001, 2002, 2003, 
-   2005 Free Software Foundation, Inc.
+   2005, 2006 Free Software Foundation, Inc.
 
    GNU Mailutils is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -877,6 +877,10 @@ main (int argc, char **argv)
 		 opt_handler, NULL, &index);
   if (has_folder)
     {
+      /* If a +folder is given along with the -all switch, folder will, in
+	 addition to setting the current folder, list the top-level
+	 subfolders for the current folder (with -norecurse) or list all
+	 sub-folders under the current folder recursively (with -recurse). */
       if (show_all && max_depth)
 	max_depth = 2;
       show_all = 0;
