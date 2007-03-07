02e2dcdda251cc4d3dbe2df78043962a22661e96
Sergey Poznyakoff <gray@gnu.org.ua>
2007-03-07 19:52:24 +0000

    (mu_assoc_create): Add third argument. (mu_assoc_destroy,mu_assoc_clear): New functions

--- include/mailutils/assoc.h
+++ include/mailutils/assoc.h
@@ -25,9 +25,14 @@ extern "C" {
 
 #include <mailutils/types.h>
 
+#define MU_ASSOC_COPY_KEY 0x01
+#define MU_ASSOC_ICASE    0x02
+  
 typedef void (*mu_assoc_free_fn) (void *data);
   
-int mu_assoc_create (mu_assoc_t *passoc, size_t elsize);
+int mu_assoc_create (mu_assoc_t *passoc, size_t elsize, int flags);
+void mu_assoc_destroy (mu_assoc_t *passoc);
+void mu_assoc_clear (mu_assoc_t assoc);  
 void *mu_assoc_ref (mu_assoc_t assoc, const char *name);
 int mu_assoc_install (mu_assoc_t assoc, const char *name, void *value);
 int mu_assoc_ref_install (mu_assoc_t assoc, const char *name, void **pval);
