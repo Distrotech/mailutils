23e39709d95eb384037de68511d83672a21daf70
Sergey Poznyakoff <gray@gnu.org.ua>
2007-06-26 13:56:12 +0000

    (_get_envelope_sender): Bugfix: the return value was wrong.

--- libmu_scm/mu_message.c
+++ libmu_scm/mu_message.c
@@ -1,5 +1,5 @@
 /* GNU Mailutils -- a suite of utilities for electronic mail
-   Copyright (C) 1999, 2000, 2001, 2006 Free Software Foundation, Inc.
+   Copyright (C) 1999, 2000, 2001, 2006, 2007 Free Software Foundation, Inc.
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
@@ -67,7 +67,7 @@ _get_envelope_sender (mu_envelope_t env)
     }
 
   mu_address_destroy (&addr);
-  return buffer;
+  return ptr;
 }
 
 static int
