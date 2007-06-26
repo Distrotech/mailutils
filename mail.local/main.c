d5802ef7f97ca0a2e283234dc45445a6f4f6a911
Sergey Poznyakoff <gray@gnu.org.ua>
2007-06-26 13:57:50 +0000

    (main): Register all mailbox formats

--- mail.local/main.c
+++ mail.local/main.c
@@ -1,5 +1,6 @@
 /* GNU Mailutils -- a suite of utilities for electronic mail
-   Copyright (C) 1999, 2000, 2001, 2002, 2005 Free Software Foundation, Inc.
+   Copyright (C) 1999, 2000, 2001, 2002, 2005, 
+   2007 Free Software Foundation, Inc.
 
    GNU Mailutils is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -392,9 +393,7 @@ main (int argc, char *argv[])
 	}
     }
 
-  /* Register local mbox formats. */
-  mu_registrar_record (mu_mbox_record); 
-  mu_registrar_record (mu_path_record);
+  mu_register_all_formats ();
   /* Possible supported mailers.  */
   mu_registrar_record (mu_sendmail_record);
   mu_registrar_record (mu_smtp_record);
