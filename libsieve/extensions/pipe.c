df7db464d035c92a61b12961d2b6bce2f0b7202b
Sergey Poznyakoff <gray@gnu.org.ua>
2007-11-10 18:24:23 +0000

    Update

--- libsieve/extensions/pipe.c
+++ libsieve/extensions/pipe.c
@@ -16,8 +16,7 @@
    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
    Boston, MA 02110-1301 USA */
 
-/* Syntax: pipe <program call: string>
-                [:envelope]
+/* Syntax: pipe [:envelope] <program call: string>
 		
    Notes/FIXME: 1. it would be nice to implement meta-variables in
                 <program call> which would expand to various
