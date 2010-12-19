2e6b4a7f2251ef9149e9f11216640a129114caa9
Sergey Poznyakoff <gray@gnu.org.ua>
2010-12-19 02:11:06 +0200

    sieve: improve "pipe" action, implement "pipe" test.
    * libmu_sieve/extensions/pipe.c: Allow to specify which parts of the
    message should be piped.
    Implement "pipe" test.
    * sieve/tests/pipeact.at: New file.
    * sieve/tests/pipetest.at: New file.
    * sieve/tests/Makefile.am (TESTSUITE_AT): Add pipeact.at
    and pipetest.at
    * sieve/tests/testsuite.at: Include pipeact.at
    and pipetest.at
    (MUT_SIEVE_EXT_TEST): Redefine MUT_SIEVE_OPTIONS.
    Set envar "cwd".
    Use cat and heredoc to create prog, instead of using AT_DATA.
    * sieve/tests/moderator.at: Update to these changes.

--- libmu_sieve/extensions/pipe.c
+++ libmu_sieve/extensions/pipe.c
@@ -41,17 +41,6 @@
 #include <mailutils/sieve.h>
 #include <mailutils/prog.h>
 
-int
-sieve_action_pipe (mu_sieve_machine_t mach, mu_list_t args, mu_list_t tags)
-{
-  int rc;
-  mu_message_t msg;
-  mu_sieve_value_t *val;
-  char *cmd;
-  mu_stream_t mstr, pstr;
-  mu_envelope_t env;
-  const char *error_diag = NULL;
-  const char *error_arg = NULL;
 #define ONERR(rc, diag, arg)						\
   if (rc)								\
     {									\
@@ -60,6 +49,25 @@ sieve_action_pipe (mu_sieve_machine_t mach, mu_list_t args, mu_list_t tags)
       break;								\
     }
 
+#define PIPE_ENVELOPE 0x01
+#define PIPE_HEADERS  0x02
+#define PIPE_BODY     0x04
+#define PIPE_ALL (PIPE_ENVELOPE | PIPE_HEADERS | PIPE_BODY)
+
+int
+sieve_pipe (mu_sieve_machine_t mach, mu_list_t args, mu_list_t tags, int test)
+{
+  int retval = 0;
+  int rc, result;
+  mu_message_t msg;
+  mu_sieve_value_t *val;
+  char *cmd;
+  mu_stream_t pstr;
+  mu_envelope_t env;
+  const char *error_diag = NULL;
+  const char *error_arg = NULL;
+  int pipe_mask = 0;
+  
   val = mu_sieve_value_get (args, 0);
   if (!val)
     {
@@ -70,10 +78,9 @@ sieve_action_pipe (mu_sieve_machine_t mach, mu_list_t args, mu_list_t tags)
     }
   cmd = val->v.string;
 
-  mu_sieve_log_action (mach, "PIPE", NULL);
   if (mu_sieve_get_debug_level (mach) & MU_SIEVE_DEBUG_TRACE)
     {
-      mu_sieve_debug (mach, "PIPE");
+      mu_sieve_debug (mach, test ? "PIPE (test)" : "PIPE (action)");
     }
 
   if (mu_sieve_is_dry_run (mach))
@@ -82,15 +89,22 @@ sieve_action_pipe (mu_sieve_machine_t mach, mu_list_t args, mu_list_t tags)
   msg = mu_sieve_get_message (mach);
   mu_message_get_envelope (msg, &env);
 
+  if (mu_sieve_tag_lookup (tags, "envelope", NULL))
+    pipe_mask |= PIPE_ENVELOPE;
+  if (mu_sieve_tag_lookup (tags, "header", NULL))
+    pipe_mask |= PIPE_HEADERS;
+  if (mu_sieve_tag_lookup (tags, "body", NULL))
+    pipe_mask |= PIPE_BODY;
+  if (pipe_mask == 0)
+    pipe_mask = PIPE_ALL;
   do
     {
-      rc = mu_message_get_streamref (msg, &mstr);
-      ONERR (rc, _("cannot get message stream"), NULL);
-
+      mu_stream_t mstr = NULL;
+      
       rc = mu_command_stream_create (&pstr, cmd, MU_STREAM_WRITE);
       ONERR (rc, _("cannot create command stream"), cmd);
 
-      if (mu_sieve_tag_lookup (tags, "envelope", &val))
+      if (pipe_mask & PIPE_ENVELOPE)
 	{
 	  char *p;
 
@@ -111,14 +125,30 @@ sieve_action_pipe (mu_sieve_machine_t mach, mu_list_t args, mu_list_t tags)
 	  ONERR (rc, _("stream write failed"), NULL);
 	}
 
-      rc = mu_stream_copy (pstr, mstr, 0, NULL);
-      ONERR (rc, _("command failed"), cmd);
+      if (pipe_mask & PIPE_HEADERS)
+	{
+	  mu_header_t hdr;
+	  
+	  mu_message_get_header (msg, &hdr);
+	  mu_header_get_streamref (hdr, &mstr);
+	  rc = mu_stream_copy (pstr, mstr, 0, NULL);
+	  ONERR (rc, _("copying headers failed"), cmd);
+	  mu_stream_destroy (&mstr);
+	}
+      if (pipe_mask & PIPE_BODY)
+	{
+	  mu_body_t body;
+
+	  mu_message_get_body (msg, &body);
+	  mu_body_get_streamref (body, &mstr);
+	  rc = mu_stream_copy (pstr, mstr, 0, NULL);
+	  ONERR (rc, _("copying body failed"), cmd);
+	  mu_stream_destroy (&mstr);
+	}
     }
   while (0);
 
-  mu_stream_destroy (&mstr);
-  mu_stream_close (pstr);
-  mu_stream_destroy (&pstr);
+  result = mu_stream_close (pstr);
 
   if (rc)
     {
@@ -133,20 +163,91 @@ sieve_action_pipe (mu_sieve_machine_t mach, mu_list_t args, mu_list_t tags)
 			(unsigned long) mu_sieve_get_message_num (mach),
 			error_diag,
 			mu_strerror (rc));
+      mu_stream_destroy (&pstr);
       mu_sieve_abort (mach);
     }
 
-  return 0;
+  if (test)
+    {
+      int code = 0;
+      int status;
+      
+      rc = mu_stream_ioctl (pstr, MU_IOCTL_PROGSTREAM,
+			    MU_IOCTL_PROG_STATUS, &status);
+      if (rc)
+	{
+	  mu_stream_destroy (&pstr);
+	  mu_sieve_abort (mach);
+	}
+
+      if (mu_sieve_tag_lookup (tags, "exit", &val))
+	code = val->v.number;
+      if (result == 0)
+	retval = code == 0;
+      else if (result == MU_ERR_PROCESS_EXITED)
+	retval = code == WEXITSTATUS (status);
+      else if (result == MU_ERR_PROCESS_SIGNALED)
+	{
+	  int signo = WTERMSIG (status);
+	  if (mu_sieve_tag_lookup (tags, "signal", &val))
+	    retval = signo == val->v.number;
+	  else
+	    {
+	      mu_stream_destroy (&pstr);
+	      mu_sieve_abort (mach);
+	    }
+	}
+      else
+	{
+	  mu_sieve_error (mach, "%lu: %s",
+			  (unsigned long) mu_sieve_get_message_num (mach),
+			  mu_strerror (result));
+	  mu_stream_destroy (&pstr);
+	  mu_sieve_abort (mach);
+	}
+    }
+	    
+  mu_stream_destroy (&pstr);
+
+  return retval;
 }
+
+int
+sieve_action_pipe (mu_sieve_machine_t mach, mu_list_t args, mu_list_t tags)
+{
+  mu_sieve_log_action (mach, "PIPE", NULL);
+  return sieve_pipe (mach, args, tags, 0);
+}
+
+int
+sieve_test_pipe (mu_sieve_machine_t mach, mu_list_t args, mu_list_t tags)
+{
+  return sieve_pipe (mach, args, tags, 1);
+}
+
 
 /* Tagged arguments: */
 static mu_sieve_tag_def_t pipe_tags[] = {
   { "envelope", SVT_VOID },
+  { "body", SVT_VOID },
+  { "header", SVT_VOID },
   { NULL }
 };
 
-static mu_sieve_tag_group_t pipe_tag_groups[] = {
+static mu_sieve_tag_group_t pipe_action_tag_groups[] = {
   { pipe_tags, NULL },
+  { NULL }
+};
+
+static mu_sieve_tag_def_t pipe_test_tags[] = {
+  { "exit", SVT_NUMBER },
+  { "signal", SVT_NUMBER },
+  { NULL }
+};
+
+static mu_sieve_tag_group_t pipe_test_tag_groups[] = {
+  { pipe_tags, NULL },
+  { pipe_test_tags, NULL },
   { NULL }
 };
 
@@ -159,6 +260,11 @@ static mu_sieve_data_type pipe_args[] = {
 int
 SIEVE_EXPORT (pipe, init) (mu_sieve_machine_t mach)
 {
-  return mu_sieve_register_action (mach, "pipe", sieve_action_pipe,
-				   pipe_args, pipe_tag_groups, 1);
+  int rc;
+  rc = mu_sieve_register_action (mach, "pipe", sieve_action_pipe,
+				 pipe_args, pipe_action_tag_groups, 1);
+  if (rc)
+    return rc;
+  return mu_sieve_register_test (mach, "pipe", sieve_test_pipe,
+				 pipe_args, pipe_test_tag_groups, 1);
 }
