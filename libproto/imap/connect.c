1b5098c98df757ad24841bcc8b5350a64c07a25a
Sergey Poznyakoff <gray@gnu.org.ua>
2011-07-05 02:12:30 +0300

    Fixes.
    * libmailutils/diag/errors: Fix typo.
    * libproto/imap/connect.c: Add missing includes.
    * mh/tests/mhn.at: Add extra argument quoting.

--- libproto/imap/connect.c
+++ libproto/imap/connect.c
@@ -21,6 +21,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/select.h>
+#include <sys/time.h>
 #include <mailutils/errno.h>
 #include <mailutils/wordsplit.h>
 #include <mailutils/sys/imap.h>
