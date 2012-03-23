Distrotech/mailutils

commit e75e9123344ff44bacdbe28d071411e6799384ec

Implement session ID for TCP/UDP servers.
* imap4d/imap4d.c (set_strerr_flt,clr_strerr_flt): New functions.
(imap4d_connection): Setup session prefix in strerr if
mu_log_session_id is set.
* imap4d/io.c (io_setio): Bugfix: unref debug stream.
* pop3d/extra.c (pop3d_setio): Likewise.
* pop3d/pop3d.c (set_strerr_flt,clr_strerr_flt): New functions.
(pop3d_connection): Setup session prefix in strerr if
mu_log_session_id is set.

* include/mailutils/acl.h (mu_acl_setenv)
(mu_acl_getenv,mu_acl_set_session_id): New functions.
* include/mailutils/server.h (mu_session_id): New extern.
(mu_m_server_configured_count): Fix signature.
* include/mailutils/syslog.h (mu_log_session_id): New extern.
* libmailutils/diag/syslog.c (mu_log_session_id): New global.

* libmailutils/server/acl.c (_mu_acl) <envv,envc,envn>: New
members.
(run_closure) <env>: New member.
(mu_acl_destroy): Free environment.
(_acl_match): Handle "family" variable.
(expand_arg): Propagate ACL environment to wordsplit.
(mu_acl_check_sockaddr): Propagate ACL environment.
(mu_acl_setenv)
(mu_acl_getenv,mu_acl_set_session_id): New functions.

* libmailutils/server/ipsrv.c (mu_ip_tcp_accept): Store session ID
in the ACL.
(mu_ip_udp_accept): Likewise.
* libmailutils/server/msrv.c (mu_m_server_configured_count): Fix
signature.
(mu_m_server_check_acl): Store session ID in the ACL.
* libmailutils/server/server.c (mu_session_id): New global variable.
(connection_loop): Increment session ID
(mu_acl_set_session_id): New function.
* libmailutils/stream/fltstream.c (filter_ctl): Handle
MU_IOCTL_SUBSTREAM.
* libmailutils/stream/logstream.c (_log_ctl): Likewise.
* libmu_cfg/common.c (mu_logging_param) <session-id>: New statement.

imap4d/imap4d.c
@@ -408,7 +408,70 @@ get_client_address (int fd, struct sockaddr_in *pcs)
     }
   return 0;
 }
+
+static int
+set_strerr_flt ()
+{
+  mu_stream_t flt, trans[2];
+  int rc;
+  
+  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_SUBSTREAM, MU_IOCTL_OP_GET, trans);
+  if (rc == 0)
+    {
+      char sessidstr[10];
+      char *argv[] = { "inline-comment", NULL, "-S", NULL };
 
+      snprintf (sessidstr, sizeof sessidstr, "%08lx:", mu_session_id);
+      argv[1] = sessidstr;
+      rc = mu_filter_create_args (&flt, trans[0], "inline-comment", 3,
+				  (const char **)argv,
+				  MU_FILTER_ENCODE, MU_STREAM_WRITE);
+      mu_stream_unref (trans[0]);
+      if (rc == 0)
+	{
+	  mu_stream_set_buffer (flt, mu_buffer_line, 0);
+	  trans[0] = flt;
+	  trans[1] = NULL;
+	  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_SUBSTREAM,
+				MU_IOCTL_OP_SET, trans);
+	  mu_stream_unref (trans[0]);
+	  if (rc)
+	    mu_error (_("%s failed: %s"), "MU_IOCTL_SET_STREAM",
+		      mu_stream_strerror (mu_strerr, rc));
+	}
+      else
+	mu_error (_("cannot create log filter stream: %s"), mu_strerror (rc));
+    }
+  else
+    {
+      mu_error (_("%s failed: %s"), "MU_IOCTL_GET_STREAM",
+		mu_stream_strerror (mu_strerr, rc));
+    }
+  return rc;
+}
+
+static void
+clr_strerr_flt ()
+{
+  mu_stream_t flt, trans[2];
+  int rc;
+
+  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_SUBSTREAM, MU_IOCTL_OP_GET, trans);
+  if (rc == 0)
+    {
+      flt = trans[0];
+
+      rc = mu_stream_ioctl (flt, MU_IOCTL_SUBSTREAM, MU_IOCTL_OP_GET, trans);
+      if (rc == 0)
+	{
+	  mu_stream_unref (trans[0]);
+	  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_SUBSTREAM,
+				MU_IOCTL_OP_SET, trans);
+	  if (rc == 0)
+	    mu_stream_unref (flt);
+	}
+    }
+}
 
 void
 imap4d_child_signal_setup (RETSIGTYPE (*handler) (int signo))
@@ -507,10 +570,21 @@ imap4d_connection (int fd, struct sockaddr *sa, int salen,
 		   struct mu_srv_config *pconf, void *data)
 {
   struct imap4d_srv_config *cfg = (struct imap4d_srv_config *) pconf;
-
+  int rc;
+  
   idle_timeout = pconf->timeout;
   imap4d_transcript = pconf->transcript;
+
+  if (mu_log_session_id)
+    rc = set_strerr_flt ();
+  else
+    rc = 1;
+  
   imap4d_mainloop (fd, fd, cfg->tls);
+
+  if (rc == 0)
+    clr_strerr_flt ();
+  
   return 0;
 }
 

libmailutils/stream/fltstream.c
@@ -421,6 +421,32 @@ filter_ctl (struct _mu_stream *stream, int code, int opcode, void *ptr)
 	  return ENOSYS;
 	}
       break;
+
+    case MU_IOCTL_SUBSTREAM:
+      if (!ptr)
+	return EINVAL;
+      else
+	{
+	  mu_stream_t *pstr = ptr;
+	  switch (opcode)
+	    {
+	    case MU_IOCTL_OP_GET:
+	      pstr[0] = fs->transport;
+	      mu_stream_ref (pstr[0]);
+	      pstr[1] = NULL;
+	      break;
+
+	    case MU_IOCTL_OP_SET:
+	      mu_stream_unref (fs->transport);
+	      fs->transport = pstr[0];
+	      mu_stream_ref (fs->transport);
+	      break;
+
+	    default:
+	      return EINVAL;
+	    }
+	}
+      break;
       
     case MU_IOCTL_TRANSPORT:
       switch (opcode)

libmailutils/stream/logstream.c
@@ -309,6 +309,32 @@ _log_ctl (struct _mu_stream *str, int code, int opcode, void *arg)
 	}
       break;
 
+    case MU_IOCTL_SUBSTREAM:
+      if (!arg)
+	return EINVAL;
+      else
+	{
+	  mu_stream_t *pstr = arg;
+	  switch (opcode)
+	    {
+	    case MU_IOCTL_OP_GET:
+	      pstr[0] = sp->transport;
+	      mu_stream_ref (pstr[0]);
+	      pstr[1] = NULL;
+	      break;
+
+	    case MU_IOCTL_OP_SET:
+	      mu_stream_unref (sp->transport);
+	      sp->transport = pstr[0];
+	      mu_stream_ref (sp->transport);
+	      break;
+
+	    default:
+	      return EINVAL;
+	    }
+	}
+      break;
+      
     case MU_IOCTL_LOGSTREAM:
       switch (opcode)
 	{

pop3d/pop3d.c
@@ -366,17 +366,90 @@ pop3d_mainloop (int ifd, int ofd, int tls)
   return status;
 }
 
+static int
+set_strerr_flt ()
+{
+  mu_stream_t flt, trans[2];
+  int rc;
+  
+  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_SUBSTREAM, MU_IOCTL_OP_GET, trans);
+  if (rc == 0)
+    {
+      char sessidstr[10];
+      char *argv[] = { "inline-comment", NULL, "-S", NULL };
+
+      snprintf (sessidstr, sizeof sessidstr, "%08lx:", mu_session_id);
+      argv[1] = sessidstr;
+      rc = mu_filter_create_args (&flt, trans[0], "inline-comment", 3,
+				  (const char **)argv,
+				  MU_FILTER_ENCODE, MU_STREAM_WRITE);
+      mu_stream_unref (trans[0]);
+      if (rc == 0)
+	{
+	  mu_stream_set_buffer (flt, mu_buffer_line, 0);
+	  trans[0] = flt;
+	  trans[1] = NULL;
+	  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_SUBSTREAM,
+				MU_IOCTL_OP_SET, trans);
+	  mu_stream_unref (trans[0]);
+	  if (rc)
+	    mu_error (_("%s failed: %s"), "MU_IOCTL_SET_STREAM",
+		      mu_stream_strerror (mu_strerr, rc));
+	}
+      else
+	mu_error (_("cannot create log filter stream: %s"), mu_strerror (rc));
+    }
+  else
+    {
+      mu_error (_("%s failed: %s"), "MU_IOCTL_GET_STREAM",
+		mu_stream_strerror (mu_strerr, rc));
+    }
+  return rc;
+}
+
+static void
+clr_strerr_flt ()
+{
+  mu_stream_t flt, trans[2];
+  int rc;
+
+  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_SUBSTREAM, MU_IOCTL_OP_GET, trans);
+  if (rc == 0)
+    {
+      flt = trans[0];
+
+      rc = mu_stream_ioctl (flt, MU_IOCTL_SUBSTREAM, MU_IOCTL_OP_GET, trans);
+      if (rc == 0)
+	{
+	  mu_stream_unref (trans[0]);
+	  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_SUBSTREAM,
+				MU_IOCTL_OP_SET, trans);
+	  if (rc == 0)
+	    mu_stream_unref (flt);
+	}
+    }
+}
+
 int
 pop3d_connection (int fd, struct sockaddr *sa, int salen,
 		  struct mu_srv_config *pconf,
 		  void *data)
 {
   struct pop3d_srv_config *cfg = (struct pop3d_srv_config *) pconf;
+  int rc;
   
   idle_timeout = pconf->timeout;
   pop3d_transcript = pconf->transcript;
 
+  if (mu_log_session_id)
+    rc = set_strerr_flt ();
+  else
+    rc = 1;
+  
   pop3d_mainloop (fd, fd, cfg->tls);
+
+  if (rc == 0)
+    clr_strerr_flt ();
   return 0;
 }
 

imap4d/io.c
@@ -76,6 +76,7 @@ io_setio (int ifd, int ofd, int tls)
       else
 	{
 	  rc = mu_xscript_stream_create (&xstr, iostream, dstr, NULL);
+	  mu_stream_unref (dstr);
 	  if (rc)
 	    mu_error (_("cannot create transcript stream: %s"),
 		      mu_strerror (rc));

include/mailutils/acl.h
@@ -63,4 +63,8 @@ int mu_acl_check_fd (mu_acl_t acl, int fd, mu_acl_result_t *pres);
 int mu_acl_action_to_string (mu_acl_action_t act, const char **pstr);
 int mu_acl_string_to_action (const char *str, mu_acl_action_t *pres);
 
+int mu_acl_setenv (mu_acl_t acl, const char *name, const char *val);
+const char *mu_acl_getenv (mu_acl_t acl, const char *name);
+int mu_acl_set_session_id (mu_acl_t acl);
+
 #endif

include/mailutils/server.h
@@ -21,6 +21,8 @@
 #include <mailutils/types.h>
 #include <signal.h>
 
+extern unsigned long mu_session_id;
+
 typedef int (*mu_conn_loop_fp) (int fd, void *conn_data, void *server_data);
 typedef void (*mu_conn_free_fp) (void *conn_data, void *server_data);
 typedef int (*mu_server_idle_fp) (void *server_data);
@@ -124,7 +126,7 @@ const char * mu_m_server_pidfile (mu_m_server_t srv);
 void mu_m_server_get_sigset (mu_m_server_t srv, sigset_t *sigset);
 int mu_m_server_get_srvlist (mu_m_server_t srv, mu_list_t *plist);
 
-void mu_m_server_configured_count (mu_m_server_t msrv, size_t count);
+void mu_m_server_configured_count (mu_m_server_t msrv, size_t *count);
 
 void mu_m_server_begin (mu_m_server_t msrv);
 int mu_m_server_run (mu_m_server_t msrv);

include/mailutils/syslog.h
@@ -29,6 +29,7 @@ extern int mu_log_facility;
 extern char *mu_log_tag;
 extern int mu_log_print_severity;
 extern unsigned mu_log_severity_threshold;
+extern int mu_log_session_id;
 
 #define MU_LOG_TAG() (mu_log_tag ? mu_log_tag : mu_program_name)
   

commit 9f9d7b0b7cdb586b34ff08bfa7c752d05088a720

Fix semantics of MU_IOCTL_SUBSTREAM.
This also fixes e75e9123, which broke TLS support in daemons.

* include/mailutils/stream.h (MU_IOCTL_TOPSTREAM): New ioctl.
* imap4d/imap4d.c (set_strerr_flt)
(clr_strerr_flt): Use MU_IOCTL_TOPSTREAM.
* pop3d/pop3d.c: Likewise.
* libmailutils/stream/fltstream.c (filter_ctl): Handle both
MU_IOCTL_SUBSTREAM and MU_IOCTL_TOPSTREAM.
* libmailutils/stream/iostream.c (_iostream_ctl): Likewise.
* libmailutils/stream/logstream.c (_log_ctl): Likewise.
* libmailutils/stream/xscript-stream.c (_xscript_ctl): Likewise.

imap4d/imap4d.c
@@ -415,7 +415,7 @@ set_strerr_flt ()
   mu_stream_t flt, trans[2];
   int rc;
   
-  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_SUBSTREAM, MU_IOCTL_OP_GET, trans);
+  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_TOPSTREAM, MU_IOCTL_OP_GET, trans);
   if (rc == 0)
     {
       char sessidstr[10];
@@ -432,7 +432,7 @@ set_strerr_flt ()
 	  mu_stream_set_buffer (flt, mu_buffer_line, 0);
 	  trans[0] = flt;
 	  trans[1] = NULL;
-	  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_SUBSTREAM,
+	  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_TOPSTREAM,
 				MU_IOCTL_OP_SET, trans);
 	  mu_stream_unref (trans[0]);
 	  if (rc)
@@ -456,16 +456,16 @@ clr_strerr_flt ()
   mu_stream_t flt, trans[2];
   int rc;
 
-  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_SUBSTREAM, MU_IOCTL_OP_GET, trans);
+  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_TOPSTREAM, MU_IOCTL_OP_GET, trans);
   if (rc == 0)
     {
       flt = trans[0];
 
-      rc = mu_stream_ioctl (flt, MU_IOCTL_SUBSTREAM, MU_IOCTL_OP_GET, trans);
+      rc = mu_stream_ioctl (flt, MU_IOCTL_TOPSTREAM, MU_IOCTL_OP_GET, trans);
       if (rc == 0)
 	{
 	  mu_stream_unref (trans[0]);
-	  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_SUBSTREAM,
+	  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_TOPSTREAM,
 				MU_IOCTL_OP_SET, trans);
 	  if (rc == 0)
 	    mu_stream_unref (flt);

include/mailutils/stream.h
@@ -73,6 +73,10 @@ enum mu_buffer_type
 #define MU_IOCTL_FD               9 /* File descriptor manipulation */
 #define MU_IOCTL_SYSLOGSTREAM    10 /* Syslog stream (see below) */
 #define MU_IOCTL_FILTER          11 /* Filter streams (see below) */
+#define MU_IOCTL_TOPSTREAM       12 /* Same as MU_IOCTL_SUBSTREAM, but
+				       always returns the topmost substream,
+				       traversing stream chains if necessary.
+				    */
   
   /* Opcodes common for various families */
 #define MU_IOCTL_OP_GET 0

libmailutils/stream/fltstream.c
@@ -398,7 +398,8 @@ static int
 filter_ctl (struct _mu_stream *stream, int code, int opcode, void *ptr)
 {
   struct _mu_filter_stream *fs = (struct _mu_filter_stream *)stream;
-
+  int status;
+  
   switch (code)
     {
     case MU_IOCTL_FILTER:
@@ -423,6 +424,13 @@ filter_ctl (struct _mu_stream *stream, int code, int opcode, void *ptr)
       break;
 
     case MU_IOCTL_SUBSTREAM:
+      if (fs->transport && 
+          ((status = mu_stream_ioctl (fs->transport, code, opcode, ptr)) == 0
+	   || status != ENOSYS))
+        return status;
+      /* fall through */
+
+    case MU_IOCTL_TOPSTREAM:
       if (!ptr)
 	return EINVAL;
       else

libmailutils/stream/iostream.c
@@ -159,6 +159,7 @@ _iostream_ctl (struct _mu_stream *str, int code, int opcode, void *arg)
       break;
 
     case MU_IOCTL_SUBSTREAM:
+    case MU_IOCTL_TOPSTREAM:
       if (!arg)
 	return EINVAL;
       else

libmailutils/stream/logstream.c
@@ -281,6 +281,7 @@ static int
 _log_ctl (struct _mu_stream *str, int code, int opcode, void *arg)
 {
   struct _mu_log_stream *sp = (struct _mu_log_stream *)str;
+  int status;
   
   switch (code)
     {
@@ -310,6 +311,13 @@ _log_ctl (struct _mu_stream *str, int code, int opcode, void *arg)
       break;
 
     case MU_IOCTL_SUBSTREAM:
+      if (sp->transport &&
+          ((status = mu_stream_ioctl (sp->transport, code, opcode, arg)) == 0 ||
+           status != ENOSYS))
+        return status;
+      /* fall through */
+
+    case MU_IOCTL_TOPSTREAM:
       if (!arg)
 	return EINVAL;
       else

libmailutils/stream/xscript-stream.c
@@ -288,6 +288,13 @@ _xscript_ctl (struct _mu_stream *str, int code, int opcode, void *arg)
       break;
 
     case MU_IOCTL_SUBSTREAM:
+      if (sp->transport &&
+          ((status = mu_stream_ioctl (sp->transport, code, opcode, arg)) == 0 ||
+            status != ENOSYS))
+        return status;
+      /* fall through */
+
+    case MU_IOCTL_TOPSTREAM:
       if (!arg)
 	return EINVAL;
       switch (opcode)

pop3d/pop3d.c
@@ -372,7 +372,7 @@ set_strerr_flt ()
   mu_stream_t flt, trans[2];
   int rc;
   
-  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_SUBSTREAM, MU_IOCTL_OP_GET, trans);
+  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_TOPSTREAM, MU_IOCTL_OP_GET, trans);
   if (rc == 0)
     {
       char sessidstr[10];
@@ -389,7 +389,7 @@ set_strerr_flt ()
 	  mu_stream_set_buffer (flt, mu_buffer_line, 0);
 	  trans[0] = flt;
 	  trans[1] = NULL;
-	  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_SUBSTREAM,
+	  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_TOPSTREAM,
 				MU_IOCTL_OP_SET, trans);
 	  mu_stream_unref (trans[0]);
 	  if (rc)
@@ -413,16 +413,16 @@ clr_strerr_flt ()
   mu_stream_t flt, trans[2];
   int rc;
 
-  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_SUBSTREAM, MU_IOCTL_OP_GET, trans);
+  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_TOPSTREAM, MU_IOCTL_OP_GET, trans);
   if (rc == 0)
     {
       flt = trans[0];
 
-      rc = mu_stream_ioctl (flt, MU_IOCTL_SUBSTREAM, MU_IOCTL_OP_GET, trans);
+      rc = mu_stream_ioctl (flt, MU_IOCTL_TOPSTREAM, MU_IOCTL_OP_GET, trans);
       if (rc == 0)
 	{
 	  mu_stream_unref (trans[0]);
-	  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_SUBSTREAM,
+	  rc = mu_stream_ioctl (mu_strerr, MU_IOCTL_TOPSTREAM,
 				MU_IOCTL_OP_SET, trans);
 	  if (rc == 0)
 	    mu_stream_unref (flt);