Distrotech/mailutils

commit 6e2a37582ae89b43438952059412ea4a994d546c

Port 0b1e16bea to imap4d
* imap4d/imap4d.c (tls_required): Remove.
(tls_mode): New variable.
(imap4d_srv_config) <tls>: Remove.
(imap4d_srv_config) <tls_mode>: New member.
(imap4d_srv_param): Change the definition of the "tls" statement.
Mark "tls-required" as deprecated.
(imap4d_mainloop): Change type of the tls argument.
Initialize session and pass it to each handler.
* imap4d/imap4d.h (imap4d_session): New struct.
(imap4d_command) <func>: Change signature. All uses changed.
(tls_mode): New enum.
(tls_required): Remove.
(tls_available): New global.
* imap4d/io.c (io_setio): Don't call tls_encryption_on, leave it to
the caller.
* imap4d/login.c (imap4d_login): Use session.
* imap4d/starttls.c (tls_available): Global. Present even if !WITH_TLS
(tls_encryption_on): Change signature. All callers updated.

imap4d/authenticate.c
@@ -90,7 +90,8 @@ imap4d_auth_capability ()
 */
 
 int
-imap4d_authenticate (struct imap4d_command *command, imap4d_tokbuf_t tok)
+imap4d_authenticate (struct imap4d_session *session,
+		     struct imap4d_command *command, imap4d_tokbuf_t tok)
 {
   char *auth_type;
   struct imap4d_auth adata;

imap4d/capability.c
@@ -68,7 +68,8 @@ print_capa (void *item, void *data)
 }
 
 int
-imap4d_capability (struct imap4d_command *command, imap4d_tokbuf_t tok)
+imap4d_capability (struct imap4d_session *session,
+	           struct imap4d_command *command, imap4d_tokbuf_t tok)
 {
   if (imap4d_tokbuf_argc (tok) != 2)
     return io_completion_response (command, RESP_BAD, "Invalid arguments");

imap4d/imap4d.c
@@ -17,6 +17,7 @@
 #include "imap4d.h"
 #include <mailutils/gsasl.h>
 #include "mailutils/libargp.h"
+#include "mailutils/kwd.h"
 #include "tcpwrap.h"
 
 mu_m_server_t server;
@@ -30,8 +31,8 @@ char *modify_homedir;           /* Expression to produce imap4d_homedir */
 int state = STATE_NONAUTH;      /* Current IMAP4 state */
 struct mu_auth_data *auth_data; 
 
+enum tls_mode tls_mode;
 int login_disabled;             /* Disable LOGIN command */
-int tls_required;               /* Require STARTTLS */
 int create_home_dir;            /* Create home directory if it does not
 				   exist */
 int home_dir_mode = S_IRUSR|S_IWUSR|S_IXUSR|S_IRGRP|S_IXGRP|S_IROTH|S_IXOTH;
@@ -93,8 +94,6 @@ static const char *imap4d_capa[] = {
   "logging",
   NULL
 };
-
-static int imap4d_mainloop (int, int, int);
 
 static error_t
 imap4d_parse_opt (int key, char *arg, struct argp_state *state)
@@ -251,14 +250,61 @@ cb_mailbox_mode (void *data, mu_config_value_t *val)
 struct imap4d_srv_config
 {
   struct mu_srv_config m_cfg;
-  int tls;
+  enum tls_mode tls_mode;
 };
 
+#ifdef WITH_TLS
+static int
+cb_tls (void *data, mu_config_value_t *val)
+{
+  int *res = data;
+  static struct mu_kwd tls_kwd[] = {
+    { "no", tls_no },
+    { "false", tls_no },
+    { "off", tls_no },
+    { "0", tls_no },
+    { "ondemand", tls_ondemand },
+    { "stls", tls_ondemand },
+    { "required", tls_required },
+    { "connection", tls_connection },
+    /* For compatibility with prior versions: */
+    { "yes", tls_connection }, 
+    { "true", tls_connection },
+    { "on", tls_connection },
+    { "1", tls_connection },
+    { NULL }
+  };
+  
+  if (mu_cfg_assert_value_type (val, MU_CFG_STRING))
+    return 1;
+
+  if (mu_kwd_xlat_name (tls_kwd, val->v.string, res))
+    mu_error (_("not a valid tls keyword: %s"), val->v.string);
+  return 0;
+}
+#endif
+
+static int
+cb_tls_required (void *data, mu_config_value_t *val)
+{
+  int *res = data;
+  int bv;
+  
+  if (mu_cfg_assert_value_type (val, MU_CFG_STRING))
+    return 1;
+  if (mu_cfg_parse_boolean (val->v.string, &bv))
+    mu_error (_("Not a boolean value"));
+  else
+    *res = tls_required;
+  return 0;
+}
+
 static struct mu_cfg_param imap4d_srv_param[] = {
-  { "tls", mu_cfg_bool, NULL, mu_offsetof (struct imap4d_srv_config, tls),
-    NULL,
-    N_("Use TLS encryption for this server")
-  },
+#ifdef WITH_TLS
+  { "tls", mu_cfg_callback,
+    NULL, mu_offsetof (struct imap4d_srv_config, tls_mode), cb_tls,
+    N_("Kind of TLS encryption to use for this server") },
+#endif
   { NULL }
 };
 
@@ -287,8 +333,13 @@ static struct mu_cfg_param imap4d_cfg_param[] = {
   { "home-dir-mode", mu_cfg_callback, NULL, 0, cb_mode,
     N_("File mode for creating user home directories (octal)."),
     N_("mode") },
-  { "tls-required", mu_cfg_bool, &tls_required, 0, NULL,
-    N_("Always require STARTTLS before entering authentication phase.") },
+#ifdef WITH_TLS
+  { "tls", mu_cfg_callback, &tls_mode, 0, cb_tls,
+    N_("Kind of TLS encryption to use") },
+  { "tls-required", mu_cfg_callback, &tls_mode, 0, cb_tls_required,
+    N_("Always require STLS before entering authentication phase.\n"
+       "Deprecated, use \"tls required\" instead.") },
+#endif
   { "preauth", mu_cfg_callback, NULL, 0, cb_preauth,
     N_("Configure PREAUTH mode.  MODE is one of:\n"
        "  prog:///<full-program-name: string>\n"
@@ -480,11 +531,12 @@ imap4d_child_signal_setup (RETSIGTYPE (*handler) (int signo))
 }
 
 static int
-imap4d_mainloop (int ifd, int ofd, int tls)
+imap4d_mainloop (int ifd, int ofd, enum tls_mode tls)
 {
   imap4d_tokbuf_t tokp;
   char *text;
   int signo;
+  struct imap4d_session session;
 
   if (!test_mode)
     test_mode = isatty (ifd);
@@ -521,7 +573,29 @@ imap4d_mainloop (int ifd, int ofd, int tls)
       imap4d_child_signal_setup (imap4d_child_signal);
     }
   
-  io_setio (ifd, ofd, tls);
+  if (tls == tls_unspecified)
+    tls = tls_available ? tls_ondemand : tls_no;
+  else if (tls != tls_no && !tls_available)
+    {
+      mu_error (_("TLS is not configured, but requested in the "
+		  "configuration"));
+      tls = tls_no;
+    }
+
+  switch (tls)
+    {
+    case tls_required:
+      imap4d_capability_add (IMAP_CAPA_XTLSREQUIRED);
+    case tls_no:
+      imap4d_capability_remove (IMAP_CAPA_STARTTLS);
+      tls_available = 0;
+    }
+  
+  session.tls_mode = tls;
+  
+  io_setio (ifd, ofd, tls == tls_connection);
+  if (tls == tls_connection)
+    tls_encryption_on (&session);
 
   if (imap4d_preauth_setup (ifd) == 0)
     {
@@ -555,7 +629,7 @@ imap4d_mainloop (int ifd, int ofd, int tls)
       imap4d_readline (tokp);
       /* check for updates */
       imap4d_sync ();
-      util_do_command (tokp);
+      util_do_command (&session, tokp);
       imap4d_sync ();
       io_flush ();
     }
@@ -578,7 +652,7 @@ imap4d_connection (int fd, struct sockaddr *sa, int salen,
   else
     rc = 1;
   
-  imap4d_mainloop (fd, fd, cfg->tls);
+  imap4d_mainloop (fd, fd, cfg->tls_mode);
 
   if (rc == 0)
     clr_strerr_flt ();
@@ -687,10 +761,6 @@ main (int argc, char **argv)
 
   if (login_disabled)
     imap4d_capability_add (IMAP_CAPA_LOGINDISABLED);
-#ifdef WITH_TLS
-  if (tls_required)
-    imap4d_capability_add (IMAP_CAPA_XTLSREQUIRED);
-#endif
 
   namespace_init ();
 

imap4d/imap4d.h
@@ -115,10 +115,13 @@ extern "C" {
 
 typedef struct imap4d_tokbuf *imap4d_tokbuf_t;
 
+struct imap4d_session;
+  
 struct imap4d_command
 {
   const char *name;
-  int (*func) (struct imap4d_command *, imap4d_tokbuf_t);
+  int (*func) (struct imap4d_session *, struct imap4d_command *,
+	       imap4d_tokbuf_t);
   int states;
   int failure;
   int success;
@@ -175,6 +178,20 @@ enum imap4d_preauth
     preauth_prog
   };
 
+  /* TLS modes */
+enum tls_mode
+  {
+    tls_unspecified,
+    tls_no,
+    tls_ondemand,
+    tls_required,
+    tls_connection
+  };
+
+struct imap4d_session
+{
+  enum tls_mode tls_mode;
+};
   
 extern struct imap4d_command imap4d_command_table[];
 extern mu_mailbox_t mbox;
@@ -190,7 +207,6 @@ extern const char *program_version;
 extern int mailbox_mode[NS_MAX];
   
 extern int login_disabled;
-extern int tls_required;
 extern enum imap4d_preauth preauth_mode;
 extern char *preauth_program;
 extern int preauth_only;
@@ -206,6 +222,8 @@ extern jmp_buf child_jmp;
 
 extern int test_mode;
 extern int silent_expunge;
+
+int tls_available;
 
 /* Input functions */
 extern mu_stream_t iostream;
@@ -271,51 +289,81 @@ int imap4d_with_parsebuf (imap4d_tokbuf_t tok, int arg,
 #define imap4d_parsebuf_data(p) ((p)->data)
 
   /* Imap4 commands */
-extern int  imap4d_append (struct imap4d_command *, imap4d_tokbuf_t);
-extern int  imap4d_authenticate (struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_append (struct imap4d_session *,
+			   struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_authenticate (struct imap4d_session *,
+				 struct imap4d_command *, imap4d_tokbuf_t);
 extern void imap4d_auth_capability (void);
-extern int  imap4d_capability (struct imap4d_command *, imap4d_tokbuf_t);
-extern int  imap4d_check (struct imap4d_command *, imap4d_tokbuf_t);
-extern int  imap4d_close (struct imap4d_command *, imap4d_tokbuf_t);
-extern int  imap4d_unselect (struct imap4d_command *, imap4d_tokbuf_t);
-extern int  imap4d_copy (struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_capability (struct imap4d_session *,
+			       struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_check (struct imap4d_session *,
+			  struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_close (struct imap4d_session *,
+			  struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_unselect (struct imap4d_session *,
+			     struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_copy (struct imap4d_session *,
+			 struct imap4d_command *, imap4d_tokbuf_t);
 extern int  imap4d_copy0 (imap4d_tokbuf_t, int isuid, char **err_text);
-extern int  imap4d_create (struct imap4d_command *, imap4d_tokbuf_t);
-extern int  imap4d_delete (struct imap4d_command *, imap4d_tokbuf_t);
-extern int  imap4d_examine (struct imap4d_command *, imap4d_tokbuf_t);
-extern int  imap4d_expunge (struct imap4d_command *, imap4d_tokbuf_t);
-extern int  imap4d_fetch (struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_create (struct imap4d_session *,
+			   struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_delete (struct imap4d_session *,
+			   struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_examine (struct imap4d_session *,
+			    struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_expunge (struct imap4d_session *,
+			    struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_fetch (struct imap4d_session *,
+			  struct imap4d_command *, imap4d_tokbuf_t);
 extern int  imap4d_fetch0 (imap4d_tokbuf_t tok, int isuid, char **err_text);
-extern int  imap4d_list (struct imap4d_command *, imap4d_tokbuf_t);
-extern int  imap4d_login (struct imap4d_command *, imap4d_tokbuf_t);
-extern int  imap4d_logout (struct imap4d_command *, imap4d_tokbuf_t);
-extern int  imap4d_noop (struct imap4d_command *, imap4d_tokbuf_t);
-extern int  imap4d_rename (struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_list (struct imap4d_session *,
+			 struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_login (struct imap4d_session *,
+			  struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_logout (struct imap4d_session *,
+			   struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_noop (struct imap4d_session *,
+			 struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_rename (struct imap4d_session *,
+			   struct imap4d_command *, imap4d_tokbuf_t);
 extern int  imap4d_preauth_setup (int fd);
-extern int  imap4d_search (struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_search (struct imap4d_session *,
+			     struct imap4d_command *, imap4d_tokbuf_t);
 extern int  imap4d_search0 (imap4d_tokbuf_t, int isuid, char **repyptr);
-extern int  imap4d_select (struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_select (struct imap4d_session *,
+			   struct imap4d_command *, imap4d_tokbuf_t);
 extern int  imap4d_select0 (struct imap4d_command *, const char *, int);
 extern int  imap4d_select_status (void);
 #ifdef WITH_TLS
-extern int  imap4d_starttls (struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_starttls (struct imap4d_session *,
+			     struct imap4d_command *, imap4d_tokbuf_t);
 extern void starttls_init (void);
-void tls_encryption_on (void);
+void tls_encryption_on (struct imap4d_session *);
 #endif /* WITH_TLS */
-extern int  imap4d_status (struct imap4d_command *, imap4d_tokbuf_t);
-extern int  imap4d_store (struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_status (struct imap4d_session *,
+			   struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_store (struct imap4d_session *,
+			  struct imap4d_command *, imap4d_tokbuf_t);
 extern int  imap4d_store0 (imap4d_tokbuf_t, int, char **);
 
 mu_property_t open_subscription (void);
-extern int  imap4d_subscribe (struct imap4d_command *, imap4d_tokbuf_t);
-extern int  imap4d_unsubscribe (struct imap4d_command *, imap4d_tokbuf_t);
-extern int  imap4d_lsub (struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_subscribe (struct imap4d_session *,
+			      struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_unsubscribe (struct imap4d_session *,
+				struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_lsub (struct imap4d_session *,
+			 struct imap4d_command *, imap4d_tokbuf_t);
 
-extern int  imap4d_uid (struct imap4d_command *, imap4d_tokbuf_t);
-extern int  imap4d_namespace (struct imap4d_command *, imap4d_tokbuf_t);
-extern int  imap4d_version (struct imap4d_command *, imap4d_tokbuf_t);
-extern int  imap4d_idle (struct imap4d_command *, imap4d_tokbuf_t);
-extern int  imap4d_id (struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_uid (struct imap4d_session *,
+			struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_namespace (struct imap4d_session *,
+			      struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_version (struct imap4d_session *,
+			    struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_idle (struct imap4d_session *,
+			 struct imap4d_command *, imap4d_tokbuf_t);
+extern int  imap4d_id (struct imap4d_session *,
+		       struct imap4d_command *, imap4d_tokbuf_t);
   
 extern int imap4d_check_home_dir (const char *dir, uid_t uid, gid_t gid);
 
@@ -364,7 +412,7 @@ extern void imap4d_capability_init (void);
 
 extern int  util_start (char *);
 extern int  util_getstate (void);
-extern int  util_do_command (imap4d_tokbuf_t);
+extern int  util_do_command (struct imap4d_session *, imap4d_tokbuf_t);
 extern char *util_getfullpath (const char *);
 extern struct imap4d_command *util_getcommand (char *, 
                                                struct imap4d_command []);

imap4d/append.c
@@ -195,7 +195,8 @@ imap4d_append0 (mu_mailbox_t mbox, int flags, char *date_time, char *text,
 
 /* APPEND mbox [(flags)] [date_time] message_literal */
 int
-imap4d_append (struct imap4d_command *command, imap4d_tokbuf_t tok)
+imap4d_append (struct imap4d_session *session,
+               struct imap4d_command *command, imap4d_tokbuf_t tok)
 {
   int i;
   char *mboxname;

imap4d/check.c
@@ -29,7 +29,8 @@
 */
 
 int
-imap4d_check (struct imap4d_command *command, imap4d_tokbuf_t tok)
+imap4d_check (struct imap4d_session *session,
+              struct imap4d_command *command, imap4d_tokbuf_t tok)
 {
   if (imap4d_tokbuf_argc (tok) != 2)
     return io_completion_response (command, RESP_BAD, "Invalid arguments");

imap4d/close.c
@@ -77,7 +77,8 @@ imap4d_close0 (struct imap4d_command *command, imap4d_tokbuf_t tok,
       EXPUNGE responses are sent. */
 
 int
-imap4d_close (struct imap4d_command *command, imap4d_tokbuf_t tok)
+imap4d_close (struct imap4d_session *session,
+              struct imap4d_command *command, imap4d_tokbuf_t tok)
 {
   return imap4d_close0 (command, tok, 1);
 }
@@ -101,7 +102,8 @@ imap4d_close (struct imap4d_command *command, imap4d_tokbuf_t tok)
       selected mailbox.
 */
 int
-imap4d_unselect (struct imap4d_command *command, imap4d_tokbuf_t tok)
+imap4d_unselect (struct imap4d_session *session,
+                 struct imap4d_command *command, imap4d_tokbuf_t tok)
 {
   return imap4d_close0 (command, tok, 0);
 }

imap4d/copy.c
@@ -34,7 +34,8 @@
  */
 
 int
-imap4d_copy (struct imap4d_command *command, imap4d_tokbuf_t tok)
+imap4d_copy (struct imap4d_session *session,
+             struct imap4d_command *command, imap4d_tokbuf_t tok)
 {
   int rc;
   char *text;

commit b5b49d266063e4b6f1f5809b3b9f9a30eb335e47

Fix bug in handling AUTHENTICATE command (imap4d). Add user/group ACLs.
The bug was introduced in commit 6e2a3758.

The ACLs allow administrators to limit access to IMAP services to
particular users or users belonging to particular groups.

* imap4d/authenticate.c (imap4d_auth_capability): Take session
pointer as argument.  Don't report authentication capabilities
if tls is required.
(imap4d_authenticate): Fix conditional.
* imap4d/capability.c (imap4d_capability): Update call to
imap4d_auth_capability.
* imap4d/imap4d.c (cb_tls_required): Set tls_mode.
(auth_deny_user_list,auth_allow_user_list)
(auth_deny_group_list,auth_allow_group_list): New statics.
(check_user_groups,imap_check_group_list): New static functions.
(imap4d_cfg_param) <allow-users,deny-users>
<allow-groups,deny-groups>: New configuration statements.
(imap4d_session_setup0): Check allow/deny lists.
* imap4d/imap4d.h (imap4d_auth_capability): Change signature.

* pop3d/pop3d.c (cb_tls_required): Set tls_mode.

* bootstrap (gnulib_extra_files): Update.

bootstrap
@@ -104,7 +104,6 @@ tests_base=tests
 # Extra files from gnulib, which override files from other sources.
 gnulib_extra_files="
 	$build_aux/install-sh
-	$build_aux/missing
 	$build_aux/mdate-sh
 	$build_aux/texinfo.tex
 	$build_aux/depcomp

imap4d/authenticate.c
@@ -78,9 +78,10 @@ _auth_try (void *item, void *data)
 }
 
 void
-imap4d_auth_capability ()
+imap4d_auth_capability (struct imap4d_session *session)
 {
-  mu_list_foreach (imap_auth_list, _auth_capa, NULL);
+  if (session->tls_mode != tls_required)
+    mu_list_foreach (imap_auth_list, _auth_capa, NULL);
 }
 
 /*
@@ -102,7 +103,7 @@ imap4d_authenticate (struct imap4d_session *session,
   
   auth_type = imap4d_tokbuf_getarg (tok, IMAP4_ARG_1);
 
-  if (tls_required)
+  if (session->tls_mode == tls_required)
     return io_completion_response (command, RESP_NO,
 			           "Command disabled: Use STARTTLS first");
   

imap4d/capability.c
@@ -78,7 +78,7 @@ imap4d_capability (struct imap4d_session *session,
 
   mu_list_foreach (capa_list, print_capa, NULL);
   
-  imap4d_auth_capability ();
+  imap4d_auth_capability (session);
   io_sendf ("\n");
 
   return io_completion_response (command, RESP_OK, "Completed");

imap4d/imap4d.c
@@ -287,16 +287,50 @@ cb_tls (void *data, mu_config_value_t *val)
 static int
 cb_tls_required (void *data, mu_config_value_t *val)
 {
-  int *res = data;
   int bv;
   
   if (mu_cfg_assert_value_type (val, MU_CFG_STRING))
     return 1;
   if (mu_cfg_parse_boolean (val->v.string, &bv))
     mu_error (_("Not a boolean value"));
-  else
-    *res = tls_required;
+  else if (bv)
+    tls_mode = tls_required;
   return 0;
+}
+
+static mu_list_t auth_deny_user_list, auth_allow_user_list;
+static mu_list_t auth_deny_group_list, auth_allow_group_list;
+
+static int
+check_user_groups (void *item, void *data)
+{
+  char *gname = item;
+  struct group *gp;
+  char **p;
+  
+  gp = getgrnam (gname);
+  if (!gp)
+    return 0;
+
+  if (gp->gr_gid == auth_data->gid)
+    return MU_ERR_EXISTS;
+
+  for (p = gp->gr_mem; *p; p++)
+    if (strcmp (*p, auth_data->name) == 0)
+      return MU_ERR_EXISTS;
+
+  return 0;
+}
+
+static int
+imap_check_group_list (mu_list_t l)
+{
+  int rc = mu_list_foreach (l, check_user_groups, NULL);
+  if (rc == MU_ERR_EXISTS) 
+    return 0;
+  else if (rc == 0)
+    return MU_ERR_NOENT;
+  return rc;
 }
 
 static struct mu_cfg_param imap4d_srv_param[] = {
@@ -309,6 +343,19 @@ static struct mu_cfg_param imap4d_srv_param[] = {
 };
 
 static struct mu_cfg_param imap4d_cfg_param[] = {
+  { "allow-users", MU_CFG_LIST_OF(mu_cfg_string), &auth_allow_user_list,
+    0, NULL, 
+    N_("Allow access to users from this list.") },
+  { "deny-users", MU_CFG_LIST_OF(mu_cfg_string), &auth_deny_user_list,
+    0, NULL, 
+    N_("Deny access to users from this list.") },
+  { "allow-groups", MU_CFG_LIST_OF(mu_cfg_string), &auth_allow_group_list,
+    0, NULL, 
+    N_("Allow access if the user group is in this list.") },
+  { "deny-groups", MU_CFG_LIST_OF(mu_cfg_string), &auth_deny_group_list,
+    0, NULL, 
+    N_("Deny access if the user group is in this list.") },
+  
   { "homedir", mu_cfg_string, &modify_homedir, 0, NULL,
     N_("Modify home directory.") },
   { "personal-namespace", MU_CFG_LIST_OF(mu_cfg_string), &namespace[NS_PRIVATE],
@@ -367,6 +414,31 @@ static struct mu_cfg_param imap4d_cfg_param[] = {
 int
 imap4d_session_setup0 ()
 {
+  if (auth_deny_user_list &&
+      mu_list_locate (auth_deny_user_list, auth_data->name, NULL) == 0)
+    {
+      mu_error (_("%s is in deny-users, rejecting"), auth_data->name);
+      return 1;
+    }
+  if (auth_allow_user_list &&
+      mu_list_locate (auth_allow_user_list, auth_data->name, NULL))
+    {
+      mu_error (_("%s is not in allow-users, rejecting"), auth_data->name);
+      return 1;
+    }
+  if (auth_deny_group_list &&
+      imap_check_group_list (auth_deny_group_list) == 0)
+    {
+      mu_error (_("%s is in deny-groups, rejecting"), auth_data->name);
+      return 1;
+    }
+  if (auth_allow_group_list &&
+      imap_check_group_list (auth_allow_group_list))
+    {
+      mu_error (_("%s is not in allow-groups, rejecting"), auth_data->name);
+      return 1;
+    }
+
   real_homedir = mu_normalize_path (mu_strdup (auth_data->dir));
   if (imap4d_check_home_dir (real_homedir, auth_data->uid, auth_data->gid))
     return 1;

imap4d/imap4d.h
@@ -293,7 +293,7 @@ extern int  imap4d_append (struct imap4d_session *,
 			   struct imap4d_command *, imap4d_tokbuf_t);
 extern int  imap4d_authenticate (struct imap4d_session *,
 				 struct imap4d_command *, imap4d_tokbuf_t);
-extern void imap4d_auth_capability (void);
+extern void imap4d_auth_capability (struct imap4d_session *);
 extern int  imap4d_capability (struct imap4d_session *,
 			       struct imap4d_command *, imap4d_tokbuf_t);
 extern int  imap4d_check (struct imap4d_session *,

pop3d/pop3d.c
@@ -176,15 +176,14 @@ cb_tls (void *data, mu_config_value_t *val)
 static int
 cb_tls_required (void *data, mu_config_value_t *val)
 {
-  int *res = data;
   int bv;
   
   if (mu_cfg_assert_value_type (val, MU_CFG_STRING))
     return 1;
   if (mu_cfg_parse_boolean (val->v.string, &bv))
     mu_error (_("Not a boolean value"));
-  else
-    *res = tls_required;
+  else if (bv)
+    tls_mode = tls_required;
   return 0;
 }
 