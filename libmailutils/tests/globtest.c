6f32dede6a5a483b4f34c462baf64ae86d15b5f9
Sergey Poznyakoff <gray@gnu.org>
2016-12-13 22:16:03 +0200

    Minor fix

--- libmailutils/tests/globtest.c
+++ libmailutils/tests/globtest.c
@@ -1,3 +1,20 @@
+/* GNU Mailutils -- a suite of utilities for electronic mail
+   Copyright (C) 1999-2001, 2004-2005, 2007, 2010-2012, 2014-2016 Free
+   Software Foundation, Inc.
+
+   GNU Mailutils is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3, or (at your option)
+   any later version.
+
+   GNU Mailutils is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with GNU Mailutils.  If not, see <http://www.gnu.org/licenses/>. */
+
 #include <config.h>
 #include <mailutils/mailutils.h>
 
@@ -85,7 +102,7 @@ main (int argc, char **argv)
 	      size_t j;
 
 	      for (j = 0; j < nmatch; j++)
-		printf ("%02d: %.*s\n", j,
+ 		printf ("%02zu: %.*s\n", j,
 			matches[j].rm_eo - matches[j].rm_so,
 			a + matches[j].rm_so);
 	    }
